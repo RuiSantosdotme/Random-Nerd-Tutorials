7c71c93a957e718ae19219554c7d37fb95d8e6a2
Rui Santos <randomnerdtutorials@gmail.com>
2014-08-20 17:06:11 +0100

    Create Ultrasonic_Sensor_HC-SR04_with_NewPing.c

---
+++ Projects/Ultrasonic_Sensor_HC-SR04_with_NewPing.c
@@ -0,0 +1,23 @@
+/*
+ * Posted on http://randomnerdtutorials.com
+ * created by http://playground.arduino.cc/Code/NewPing
+*/
+
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN 12
+#define ECHO_PIN 11
+#define MAX_DISTANCE 200
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+ 
+void setup() {
+   Serial.begin(9600);
+}
+ 
+void loop() {
+   delay(50);
+   unsigned int uS = sonar.ping_cm();
+   Serial.print(uS);
+   Serial.println(“cm”);
+}
