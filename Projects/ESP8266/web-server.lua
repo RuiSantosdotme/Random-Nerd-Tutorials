ac43691b97b1a2f7e43d962f535d4e090bdd8419
Rui Santos <ee11077@fe.up.pt>
2015-05-06 13:24:00 +0100

    Create web-server.lua

---
+++ Projects/ESP8266/web-server.lua
@@ -0,0 +1,54 @@
+wifi.setmode(wifi.STATION)
+wifi.sta.config("YOUR_NETWORK_NAME","YOUR_NETWORK_PASSWORD")
+print(wifi.sta.getip())
+led1 = 3
+led2 = 4
+gpio.mode(led1, gpio.OUTPUT)
+gpio.mode(led2, gpio.OUTPUT)
+srv=net.createServer(net.TCP)
+srv:listen(80,function(conn)
+    conn:on("receive", function(client,request)
+        local buf = "";
+        local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
+        if(method == nil)then
+            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
+        end
+        local _GET = {}
+        if (vars ~= nil)then
+            for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
+                _GET[k] = v
+            end
+        end
+        buf = buf.."<head>";
+        buf = buf.."<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+        buf = buf.."<script src=\"https://code.jquery.com/jquery-2.1.3.min.js\"></script>";
+        buf = buf.."<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css\">";
+        buf = buf.."</head><div class=\"container\">";
+
+        buf = buf.."<h1>Web Server</h1>";
+        buf = buf.."<h2>GPIO 0</h2>";
+        buf = buf.."<div class=\"row\">";
+        buf = buf.."<div class=\"col-md-2\"><a href=\"?pin=ON1\" class=\"btn btn-block btn-lg btn-success\" role=\"button\">ON</a></div>";
+        buf = buf.."<div class=\"col-md-2\"><a href=\"?pin=OFF1\" class=\"btn btn-block btn-lg btn-danger\" role=\"button\">OFF</a></div>";
+        buf = buf.."</div>";
+        buf = buf.."<h2>GPIO 2</h2>";
+        buf = buf.."<div class=\"row\">";
+        buf = buf.."<div class=\"col-md-2\"><a href=\"?pin=ON2\" class=\"btn btn-block btn-lg btn-primary\" role=\"button\">ON</a></div>";
+        buf = buf.."<div class=\"col-md-2\"><a href=\"?pin=OFF2\" class=\"btn btn-block btn-lg btn-warning\" role=\"button\">OFF</a></div>";
+        buf = buf.."</div></div>";
+        
+        local _on,_off = "",""
+        if(_GET.pin == "ON1")then
+              gpio.write(led1, gpio.HIGH);
+        elseif(_GET.pin == "OFF1")then
+              gpio.write(led1, gpio.LOW);
+        elseif(_GET.pin == "ON2")then
+              gpio.write(led2, gpio.HIGH);
+        elseif(_GET.pin == "OFF2")then
+              gpio.write(led2, gpio.LOW);
+        end
+        client:send(buf);
+        client:close();
+        collectgarbage();
+    end)
+end)
