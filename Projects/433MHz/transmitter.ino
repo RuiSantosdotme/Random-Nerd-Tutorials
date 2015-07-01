1302b0c0a86905759ecf5f59ef72b05cef3f0f1f
Rui Santos <ee11077@fe.up.pt>
2015-07-01 11:02:57 +0100

    Create transmitter.ino

---
+++ Projects/433MHz/transmitter.ino
@@ -0,0 +1,19 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600);	  // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+    const char *msg = "Hello World!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1000);
+}
