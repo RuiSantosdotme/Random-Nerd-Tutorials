334018ff75b012acc1520716c4197288d025f881
Rui Santos <ee11077@fe.up.pt>
2015-08-11 11:02:14 +0100

    Update maindht.lua

--- Projects/ESP8266/maindht.lua
+++ Projects/ESP8266/maindht.lua
@@ -42,8 +42,9 @@ srv=net.createServer(net.TCP) srv:listen(80,function(conn)
     --generates HTML web site
     conn:send('HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nCache-Control: private, no-store\r\n\r\n\
         <!DOCTYPE HTML>\
-        <html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>\
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">\
+        <html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head>\
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">\
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">\
         <meta http-equiv="refresh" content="6">\
         </head><div class="container">\
         <h1>Sensor Data</h1></br><div class="row">\
@@ -53,8 +54,8 @@ srv=net.createServer(net.TCP) srv:listen(80,function(conn)
         </div></div></div></div>\
         <div class="col-md-4"><div class="panel panel-info"><div class="panel-heading"><h3 class="panel-title">Temperature</h3>\
         </div><div class="panel-body">\
-        <div class="form-group form-group-lg"><input type="text" class="form-control" value="'..temp..' º C">\
-        <input type="text" class="form-control" value="'..fare..' º F">\
+        <div class="form-group form-group-lg"><input type="text" class="form-control" value="'..temp..' deg C">\
+        <input type="text" class="form-control" value="'..fare..' deg F">\
         </div></div></div></div></div></div></html>')
     conn:on("sent",function(conn) conn:close() end)
     end)
