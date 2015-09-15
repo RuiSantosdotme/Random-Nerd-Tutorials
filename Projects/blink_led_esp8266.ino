8d8e7d8e0c8a0fbb1808de24120cf0a9b920d61f
Rui Santos <ee11077@fe.up.pt>
2015-09-15 15:06:07 +0100

    Create blink_led_esp8266.ino

---
+++ Projects/blink_led_esp8266.ino
@@ -0,0 +1,19 @@
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+*********/
+
+int pin = 2;
+
+void setup() {
+  // initialize GPIO 2 as an output.
+  pinMode(pin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(pin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+}
