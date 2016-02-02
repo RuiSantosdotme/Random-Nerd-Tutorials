f2d1e033c0a94be6e5e3b95dee2211a17ab80b34
Rui Santos <ee11077@fe.up.pt>
2016-02-02 12:40:17 +0000

    Create ESP8266_Web_Server_Arduino_IDE.ino

---
+++ Projects/ESP8266_Web_Server_Arduino_IDE.ino
@@ -0,0 +1,80 @@
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+*********/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+MDNSResponder mdns;
+
+// Replace with your network credentials
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+ESP8266WebServer server(80);
+
+String webPage = "";
+ 
+void setup(void){
+  webPage += "<h1>ESP8266 Web Server</h1><p>Socket #1 <a href=\"socket1On\"><button>ON</button></a>&nbsp;<a href=\"socket1Off\"><button>OFF</button></a></p>";
+  webPage += "<p>Socket #2 <a href=\"socket2On\"><button>ON</button></a>&nbsp;<a href=\"socket2Off\"><button>OFF</button></a></p>";
+  
+  // preparing GPIOs
+  pinMode(gpio0_pin, OUTPUT);
+  digitalWrite(gpio0_pin, LOW);
+  pinMode(gpio2_pin, OUTPUT);
+  digitalWrite(gpio2_pin, LOW);
+  
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  if (mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("MDNS responder started");
+  }
+  
+  server.on("/", [](){
+    server.send(200, "text/html", webPage);
+  });
+  server.on("/socket1On", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(gpio0_pin, HIGH);
+    delay(1000);
+  });
+  server.on("/socket1Off", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(gpio0_pin, LOW);
+    delay(1000); 
+  });
+  server.on("/socket2On", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(gpio2_pin, HIGH);
+    delay(1000);
+  });
+  server.on("/socket2Off", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(gpio2_pin, LOW);
+    delay(1000); 
+  });
+  server.begin();
+  Serial.println("HTTP server started");
+}
+ 
+void loop(void){
+  server.handleClient();
+} 
