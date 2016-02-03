879e10775d15224abb9c21edcdccb2feb348b89f
Rui Santos <ee11077@fe.up.pt>
2016-02-03 10:33:12 +0000

    Create Magnetic_Contact_Switch.ino

---
+++ Projects/Magnetic_Contact_Switch.ino
@@ -0,0 +1,34 @@
+/*
+  
+ Created by Rui Santos
+ 
+ All the resources for this project:
+ http://randomnerdtutorials.com/
+ 
+*/
+
+int ledOpen=8;
+int ledClose=10;
+int switchReed=6;
+
+void setup(){
+  pinMode(ledOpen, OUTPUT);
+  pinMode(ledClose, OUTPUT);
+  pinMode(switchReed, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  if (digitalRead(switchReed)==HIGH){
+    digitalWrite(ledOpen, LOW);
+    digitalWrite(ledClose, HIGH);
+    Serial.println("Your Door is Closed");
+  }
+  else {
+    digitalWrite(ledOpen, HIGH);
+    digitalWrite(ledClose, LOW);
+    Serial.println("Your Door is Open");
+  }
+  delay(1);
+}
