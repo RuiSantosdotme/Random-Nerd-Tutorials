ad6b74959852bd2e4f7cb4c1558c0a7fef6e99f9
Rui Santos <ee11077@fe.up.pt>
2016-04-13 15:15:39 +0100

    Create ESP8266_Controller.lua

---
+++ Projects/ESP8266_Controller.lua
@@ -0,0 +1,40 @@
+-- Rui Santos
+-- Complete project details at http://randomnerdtutorials.com
+
+wifi.setmode(wifi.STATION)
+wifi.sta.config("YOUR_NETWORK_NAME","YOUR_NETWORK_PASSWORD")
+print(wifi.sta.getip())
+led1 = 3
+led2 = 4
+gpio.mode(led1, gpio.OUTPUT)
+gpio.mode(led2, gpio.OUTPUT)
+srv=net.createServer(net.TCP)
+srv:listen(80,function(conn)
+    conn:on("receive", function(client,request)
+        local buf = "";
+        buf = buf.."HTTP/1.1 200 OK\n\n"
+        local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
+        if(method == nil)then
+            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
+        end
+        local _GET = {}
+        if (vars ~= nil)then
+            for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
+                _GET[k] = v
+            end
+        end
+        
+        if(_GET.pin == "ON1")then
+              gpio.write(led1, gpio.HIGH);
+        elseif(_GET.pin == "OFF1")then
+              gpio.write(led1, gpio.LOW);
+        elseif(_GET.pin == "ON2")then
+              gpio.write(led2, gpio.HIGH);
+        elseif(_GET.pin == "OFF2")then
+              gpio.write(led2, gpio.LOW);
+        end
+        client:send(buf);
+        client:close();
+        collectgarbage();
+    end)
+end)
