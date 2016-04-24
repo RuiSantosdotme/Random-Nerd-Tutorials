da8a2d7a7aa0ec11f2d6a6249276c20b95ad25b0
Rui Santos <ee11077@fe.up.pt>
2016-04-24 15:42:34 +0100

    Create smoke_detector.ino

---
+++ Projects/smoke_detector.ino
@@ -0,0 +1,46 @@
+/*******
+ 
+ All the resources for this project:
+ http://randomnerdtutorials.com/
+
+*******/
+
+int redLed = 12;
+int greenLed = 11;
+int buzzer = 10;
+int smokeD0 = 2;
+int smokeA0 = A5;
+// Your threshold value
+int sensorThres = 400;
+
+void setup() {
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  pinMode(smokeD0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int digitalSensor = digitalRead(smokeD0);
+  int analogSensor = analogRead(smokeA0);
+
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+  // Checks if it has reached the threshold value
+  if (analogSensor > sensorThres)
+  {
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    tone(buzzer, 1000, 200);
+  }
+  else
+  {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    noTone(buzzer);
+  }
+  delay(100);
+ 
+}
