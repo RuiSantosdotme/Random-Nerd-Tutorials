c1923140414e5d52cef3c66c9a5cbe4a38c2a1dc
Rui Santos <ee11077@fe.up.pt>
2016-06-30 10:45:03 +0100

    comment fixed

--- Projects/ESP8266_dht_arduino_ide.ino
+++ Projects/ESP8266_dht_arduino_ide.ino
@@ -7,7 +7,7 @@
 #include <ESP8266WiFi.h>
 #include "DHT.h"
 
-// Uncomment one of the lines bellow for whatever DHT sensor type you're using!
+// Uncomment one of the lines below for whatever DHT sensor type you're using!
 #define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
