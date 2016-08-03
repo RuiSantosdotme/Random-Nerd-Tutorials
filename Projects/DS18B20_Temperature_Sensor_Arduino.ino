182b6b00348e21eb5fa0806c0ad77bc8cc888f80
Rui Santos <ee11077@fe.up.pt>
2016-08-03 23:23:23 +0100

    Create DS18B20_Temperature_Sensor_Arduino.ino

---
+++ Projects/DS18B20_Temperature_Sensor_Arduino.ino
@@ -0,0 +1,38 @@
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+  Based on the Dallas Temperature Libary example
+*********/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is conntec to the Arduino digital pin 2
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // Start serial communication for debugging purposes
+  Serial.begin(9600);
+  // Start up the library
+  sensors.begin();
+}
+
+
+void loop(void){ 
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Celsius temperature: ");
+  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.print(" - Fahrenheit temperature: ");
+  Serial.println(sensors.getTempFByIndex(0));
+  delay(1000);
+}
