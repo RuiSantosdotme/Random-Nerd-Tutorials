601b551b17f45cb2e5275a47d842f2b76ff7b3b7
Rui Santos <ee11077@fe.up.pt>
2016-11-17 21:42:35 +0000

    Create Arduino_microphone_sensor.ino

---
+++ Projects/Arduino_microphone_sensor.ino
@@ -0,0 +1,26 @@
+/*
+ * Rui Santos 
+ * Complete Project Details http://randomnerdtutorials.com
+*/
+
+int ledPin=13;
+int sensorPin=7;
+boolean val =0;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin (9600);
+}
+  
+void loop (){
+  val =digitalRead(sensorPin);
+  Serial.println (val);
+  // when the sensor detects a signal above the threshold value, LED flashes
+  if (val==HIGH) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
