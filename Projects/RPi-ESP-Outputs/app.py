8b2475e66961f79132579a1d29b20fa7e2a9fa89
Rui Santos <ee11077@fe.up.pt>
2017-02-01 01:25:32 +0000

    Create app.py

---
+++ Projects/RPi-ESP-Outputs/app.py
@@ -0,0 +1,55 @@
+#
+# Created by Rui Santos
+# Complete project details: http://randomnerdtutorials.com
+#
+
+import paho.mqtt.client as mqtt
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+mqttc=mqtt.Client()
+mqttc.connect("localhost",1883,60)
+mqttc.loop_start()
+
+# Create a dictionary called pins to store the pin number, name, and pin state:
+pins = {
+   4 : {'name' : 'GPIO 4', 'board' : 'esp8266', 'topic' : 'esp8266/4', 'state' : 'False'},
+   5 : {'name' : 'GPIO 5', 'board' : 'esp8266', 'topic' : 'esp8266/5', 'state' : 'False'}
+   }
+
+# Put the pin dictionary into the template data dictionary:
+templateData = {
+   'pins' : pins
+   }
+
+@app.route("/")
+def main():
+   # Pass the template data into the template main.html and return it to the user
+   return render_template('main.html', **templateData)
+
+# The function below is executed when someone requests a URL with the pin number and action in it:
+@app.route("/<board>/<changePin>/<action>")
+
+def action(board, changePin, action):
+   # Convert the pin from the URL into an integer:
+   changePin = int(changePin)
+   # Get the device name for the pin being changed:
+   devicePin = pins[changePin]['name']
+   # If the action part of the URL is "on," execute the code indented below:
+   if action == "1" and board == 'esp8266':
+      mqttc.publish(pins[changePin]['topic'],"1")
+      pins[changePin]['state'] = 'True'
+
+   if action == "0" and board == 'esp8266':
+      mqttc.publish(pins[changePin]['topic'],"0")
+      pins[changePin]['state'] = 'False'
+
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'pins' : pins
+   }
+
+   return render_template('main.html', **templateData)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=8181, debug=True)
