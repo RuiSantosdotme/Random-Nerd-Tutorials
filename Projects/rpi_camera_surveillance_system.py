f145851dd8073d5cdb1f09a371588d3ace2d3691
Rui Santos <ee11077@fe.up.pt>
2017-10-31 15:45:24 +0000

    comment fixed rpi camera surveillance system

--- Projects/rpi_camera_surveillance_system.py
+++ Projects/rpi_camera_surveillance_system.py
@@ -83,7 +83,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
-    #Uncomment the next line to rotation to your Pi Camera rotation in degrees
+    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
     camera.start_recording(output, format='mjpeg')
     try:
