9cd974c9d2e957b7a205367a5ae47c476e1b0ecc
Rui Santos <ee11077@fe.up.pt>
2017-12-20 16:53:46 +0000

    Binary codes added

--- Projects/decode-rf-arduino/SendDemoExample.ino
+++ Projects/decode-rf-arduino/SendDemoExample.ino
@@ -13,19 +13,25 @@ void setup() {
   mySwitch.enableTransmit(10);
 
   // Optional set pulse length.
-  mySwitch.setPulseLength(320);
+  mySwitch.setPulseLength(REPLACE_WITH_YOUR_PULSE_LENGTH);
   
   // Optional set protocol (default is 1, will work for most outlets)
-  mySwitch.setProtocol(1);
+  mySwitch.setProtocol(REPLACE_WITH_YOUR_PROTOCOL);
   
   // Optional set number of transmission repetitions.
   // mySwitch.setRepeatTransmit(15);
 }
 
 void loop() {
-  // Binary code 
+  // Binary code - button 3
   mySwitch.send("000101010101000101010101");
   delay(1000);  
   mySwitch.send("000101010101000101010100");
+  delay(1000);
+  
+  // Binary code - button 4
+  mySwitch.send("000101010101010001010101");
+  delay(1000);  
+  mySwitch.send("000101010101010001010100");
   delay(1000);
 }
