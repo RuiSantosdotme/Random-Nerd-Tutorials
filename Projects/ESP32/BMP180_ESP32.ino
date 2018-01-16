8613bff9b97334612391bfc5e6b23846ffbf3a81
Rui Santos <ee11077@fe.up.pt>
2018-01-16 14:01:41 +0000

    BMP180_ESP32

---
+++ Projects/ESP32/BMP180_ESP32.ino
@@ -0,0 +1,48 @@
+/*
+ * Rui Santos 
+ * Complete Project Details http://randomnerdtutorials.com
+*/
+
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+  
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085/BMP180 sensor, check wiring!");
+	while (1) {}
+  }
+}
+  
+void loop() {
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+    
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+    
+  // Calculate altitude assuming 'standard' barometric
+  // pressure of 1013.25 millibar = 101325 Pascal
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
+
+  // you can get a more precise measurement of altitude
+  // if you know the current sea level pressure which will
+  // vary with weather and such. If it is 1015 millibars
+  // that is equal to 101500 Pascals.
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(102000));
+  Serial.println(" meters");
+    
+  Serial.println();
+  delay(500);
+}
