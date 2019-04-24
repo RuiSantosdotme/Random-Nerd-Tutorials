3bcc53c9b1cecadfceb53fc3be61e7a28d006a4a
Rui Santos <randomnerdtutorials@gmail.com>
2019-04-24 17:30:07 +0100

    Create esp_dht_web_server_main.py

---
+++ Projects/ESP-MicroPython/esp_dht_web_server_main.py
@@ -0,0 +1,81 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+def read_sensor():
+  global temp, temp_percentage, hum
+  temp = temp_percentage = hum = 0
+  try:
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    if (isinstance(temp, float) and isinstance(hum, float)) or (isinstance(temp, int) and isinstance(hum, int)):
+      msg = (b'{0:3.1f},{1:3.1f}'.format(temp, hum))
+
+      temp_percentage = (temp+6)/(40+6)*(100)
+      # uncomment for Fahrenheit
+      #temp = temp * (9/5) + 32.0
+      #temp_percentage = (temp-21)/(104-21)*(100)
+
+      hum = round(hum, 2)
+      return(msg)
+    else:
+      return('Invalid sensor readings.')
+  except OSError as e:
+    return('Failed to read sensor.')
+
+def web_page():
+  html = """<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP32 DHT Server</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="dht-labels">Temperature</span> 
+    <span>"""+str(temp)+"""</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i> 
+    <span class="dht-labels">Humidity</span>
+    <span>"""+str(hum)+"""</span>
+    <sup class="units">%</sup>
+  </p>
+</body>
+</html>"""
+  return html
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  print('Content = %s' % str(request))
+  sensor_readings = read_sensor()
+  print(sensor_readings)
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
