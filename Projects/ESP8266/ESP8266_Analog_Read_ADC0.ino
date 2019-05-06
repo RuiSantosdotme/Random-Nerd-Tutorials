a2bd650c2705a7b90ffb7b037cd64aa5d205d706
Rui Santos <randomnerdtutorials@gmail.com>
2019-05-06 14:01:19 +0100

    Create ESP8266_Analog_Read_ADC0.ino

---
+++ Projects/ESP8266/ESP8266_Analog_Read_ADC0.ino
@@ -0,0 +1,30 @@
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+*********/
+
+const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+
+int sensorValue = 0;  // value read from the pot
+int outputValue = 0;  // value to output to a PWM pin
+
+void setup() {
+  // initialize serial communication at 115200
+  Serial.begin(115200);
+}
+
+void loop() {
+  // read the analog in value
+  sensorValue = analogRead(analogInPin);
+  
+  // map it to the range of the PWM out
+  outputValue = map(sensorValue, 0, 1024, 0, 255);
+  
+  // print the readings in the Serial Monitor
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+
+  delay(1000);
+}
