fe7b44cbdb330483eff83933450711d0e7979b00
Rui Santos <randomnerdtutorials@gmail.com>
2019-05-21 15:48:50 +0100

    changed delay to 3 seconds

--- Projects/OLED/oled_draw_shapes.ino
+++ Projects/OLED/oled_draw_shapes.ino
@@ -28,61 +28,60 @@ void setup() {
   // Draw a single pixel in white
   display.drawPixel(64, 32, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
 
   // Draw line
   display.clearDisplay();
   display.drawLine(0, 0, 127, 20, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
   
   // Draw rectangle
   display.clearDisplay();
   display.drawRect(30, 10, 50, 30, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
   // Fill rectangle
   display.fillRect(30, 10, 50, 30, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
 
   // Draw round rectangle
   display.clearDisplay();
   display.drawRoundRect(10, 10, 30, 50, 2, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
   // Fill round rectangle
   display.clearDisplay();
   display.fillRoundRect(10, 10, 30, 50, 2, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
   
   // Draw circle
   display.clearDisplay();
   display.drawCircle(64, 32, 10, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
   // Fill circle
   display.fillCircle(64, 32, 10, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
   
   // Draw triangle
   display.clearDisplay();
   display.drawTriangle(10, 10, 55, 20, 5, 40, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
   // Fill triangle
   display.fillTriangle(10, 10, 55, 20, 5, 40, WHITE);
   display.display();
-  delay(30000);
+  delay(3000);
 
   // Invert and restore display, pausing in-between
   display.invertDisplay(true);
-  delay(30000);
+  delay(3000);
   display.invertDisplay(false);
-  delay(30000);
-
+  delay(3000);
 }
 
 void loop() {
