100dc20b00095732dda497350f1645464f165f34
Rui Santos <randomnerdtutorials@gmail.com>
2019-05-21 15:00:49 +0100

    Create oled_set_different_fonts.ino

---
+++ Projects/OLED/oled_set_different_fonts.ino
@@ -0,0 +1,37 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSerif9pt7b.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println("SSD1306 allocation failed");
+    for(;;);
+  }
+  delay(2000);
+
+  display.setFont(&FreeSerif9pt7b);
+  display.clearDisplay();
+  display.setTextSize(1);             
+  display.setTextColor(WHITE);        
+  display.setCursor(0,20);             
+  display.println("Hello, world!");
+  display.display();
+  delay(2000); 
+}
+void loop() {
+  
+}
