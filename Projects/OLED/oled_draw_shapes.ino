757ddb36dc88a83f6ccae3964ad3b1dabe187746
Rui Santos <randomnerdtutorials@gmail.com>
2019-05-21 15:02:00 +0100

    Create oled_draw_shapes.ino

---
+++ Projects/OLED/oled_draw_shapes.ino
@@ -0,0 +1,90 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(64, 32, WHITE);
+  display.display();
+  delay(30000);
+
+  // Draw line
+  display.clearDisplay();
+  display.drawLine(0, 0, 127, 20, WHITE);
+  display.display();
+  delay(30000);
+  
+  // Draw rectangle
+  display.clearDisplay();
+  display.drawRect(30, 10, 50, 30, WHITE);
+  display.display();
+  delay(30000);
+  // Fill rectangle
+  display.fillRect(30, 10, 50, 30, WHITE);
+  display.display();
+  delay(30000);
+
+  // Draw round rectangle
+  display.clearDisplay();
+  display.drawRoundRect(10, 10, 30, 50, 2, WHITE);
+  display.display();
+  delay(30000);
+  // Fill round rectangle
+  display.clearDisplay();
+  display.fillRoundRect(10, 10, 30, 50, 2, WHITE);
+  display.display();
+  delay(30000);
+  
+  // Draw circle
+  display.clearDisplay();
+  display.drawCircle(64, 32, 10, WHITE);
+  display.display();
+  delay(30000);
+  // Fill circle
+  display.fillCircle(64, 32, 10, WHITE);
+  display.display();
+  delay(30000);
+  
+  // Draw triangle
+  display.clearDisplay();
+  display.drawTriangle(10, 10, 55, 20, 5, 40, WHITE);
+  display.display();
+  delay(30000);
+  // Fill triangle
+  display.fillTriangle(10, 10, 55, 20, 5, 40, WHITE);
+  display.display();
+  delay(30000);
+
+  // Invert and restore display, pausing in-between
+  display.invertDisplay(true);
+  delay(30000);
+  display.invertDisplay(false);
+  delay(30000);
+
+}
+
+void loop() {
+  
+}
