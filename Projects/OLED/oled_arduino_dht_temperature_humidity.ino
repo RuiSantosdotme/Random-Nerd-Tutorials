7cd466a2e6c95fc8d491d1742163a3670cdd8662
Rui Santos <randomnerdtutorials@gmail.com>
2019-05-23 12:28:01 +0100

    DHT11 sensor selected

--- Projects/OLED/oled_arduino_dht_temperature_humidity.ino
+++ Projects/OLED/oled_arduino_dht_temperature_humidity.ino
@@ -18,8 +18,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
 
 // Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
-#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 DHT dht(DHTPIN, DHTTYPE);
