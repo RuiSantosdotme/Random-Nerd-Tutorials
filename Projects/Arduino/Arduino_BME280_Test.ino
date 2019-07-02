a2dc7458586d90cab1dba54cd2726536f63698d5
Rui Santos <randomnerdtutorials@gmail.com>
2019-07-02 13:21:23 +0100

    Create Arduino_BME280_Test.ino

---
+++ Projects/Arduino/Arduino_BME280_Test.ino
@@ -0,0 +1,69 @@
+/*
+ * Complete Project Details http://randomnerdtutorials.com
+*/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+//Adafruit_BME280 bme(BME_CS); // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
+unsigned long delayTime;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BME280 test"));
+
+  bool status;
+  
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin();  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  
+  Serial.println("-- Default Test --");
+  delayTime = 1000;
+
+  Serial.println();
+}
+
+
+void loop() { 
+  printValues();
+  delay(delayTime);
+}
+
+
+void printValues() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+  
+  Serial.print("Pressure = ");
+  
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+  
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+  
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+  
+  Serial.println();
+}
