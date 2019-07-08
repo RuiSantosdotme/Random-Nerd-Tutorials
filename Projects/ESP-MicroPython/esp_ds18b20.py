af956c8c4114dd146b7cd1e9ba763681b04501e4
Rui Santos <randomnerdtutorials@gmail.com>
2019-07-08 17:22:30 +0100

    Create esp_ds18b20.py

---
+++ Projects/ESP-MicroPython/esp_ds18b20.py
@@ -0,0 +1,17 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import machine, onewire, ds18x20, time
+
+ds_pin = machine.Pin(4)
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+roms = ds_sensor.scan()
+print('Found DS devices: ', roms)
+
+while True:
+  ds_sensor.convert_temp()
+  time.sleep_ms(750)
+  for rom in roms:
+    print(rom)
+    print(ds_sensor.read_temp(rom))
+  time.sleep(5)
