9653e5d9ecceb6e17a68377ccbf6db4b7409bd25
Rui Santos <randomnerdtutorials@gmail.com>
2019-07-11 18:16:22 +0100

    Update esp32_lolin32_ssd1306_oled.py

--- Projects/ESP-MicroPython/esp32_lolin32_ssd1306_oled.py
+++ Projects/ESP-MicroPython/esp32_lolin32_ssd1306_oled.py
@@ -4,7 +4,7 @@ from machine import Pin, I2C
 import ssd1306
 from time import sleep
 
-# Start I2C Communication SDA = 5 and SCL = 4 on Wemos Lolin32 ESP32 with built-in SSD1306 OLED
+# Start I2C Communication SCL = 4 and SDA = 5 on Wemos Lolin32 ESP32 with built-in SSD1306 OLED
 i2c = I2C(-1, scl=Pin(4), sda=Pin(5))
 
 oled_width = 128
