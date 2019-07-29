7b221c5609152b291f9a74925ea9c4fa4f6b0f18
Rui Santos <randomnerdtutorials@gmail.com>
2019-07-29 11:48:14 +0100

    Update ESP8266_Web_Server_Arduino_IDE_updated.ino

--- Projects/ESP8266_Web_Server_Arduino_IDE_updated.ino
+++ Projects/ESP8266_Web_Server_Arduino_IDE_updated.ino
@@ -24,6 +24,13 @@ String output4State = "off";
 const int output5 = 5;
 const int output4 = 4;
 
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
@@ -55,7 +62,10 @@ void loop(){
   if (client) {                             // If a new client connects,
     Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
