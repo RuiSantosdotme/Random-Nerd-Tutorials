db14315909e5d73726c7ab3a600c4d595de094a8
Rui Santos <randomnerdtutorials@gmail.com>
2019-09-17 13:38:27 +0100

    Create esp_access_point_boot.py

---
+++ Projects/ESP-MicroPython/esp_access_point_boot.py
@@ -0,0 +1,45 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'MicroPython-AP'
+password = '123456789'
+
+ap = network.WLAN(network.AP_IF)
+ap.config(essid=ssid, password=password)
+ap.active(True)
+
+while ap.active() == False:
+  pass
+
+print('Connection successful')
+print(ap.ifconfig())
+
+def web_page():
+  html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+  <body><h1>Hello, World!</h1></body></html>"""
+  return html
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  print('Content = %s' % str(request))
+  response = web_page()
+  conn.send(response)
+  conn.close()
