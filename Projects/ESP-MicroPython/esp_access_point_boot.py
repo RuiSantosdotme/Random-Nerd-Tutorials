c339a061f86a4c5501afd0320276b76a89d38857
Rui Santos <randomnerdtutorials@gmail.com>
2019-09-21 11:12:24 +0100

    Update esp_access_point_boot.py

--- Projects/ESP-MicroPython/esp_access_point_boot.py
+++ Projects/ESP-MicroPython/esp_access_point_boot.py
@@ -17,8 +17,8 @@ ssid = 'MicroPython-AP'
 password = '123456789'
 
 ap = network.WLAN(network.AP_IF)
-ap.config(essid=ssid, password=password)
 ap.active(True)
+ap.config(essid=ssid, password=password)
 
 while ap.active() == False:
   pass
