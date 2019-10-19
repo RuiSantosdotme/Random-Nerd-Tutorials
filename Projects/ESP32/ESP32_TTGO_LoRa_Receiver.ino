fc434889e3cc5190c47c3b02ca95bb89477d6b38
Rui Santos <randomnerdtutorials@gmail.com>
2019-10-19 15:10:51 +0100

    Create ESP32_TTGO_LoRa_Receiver.ino

---
+++ Projects/ESP32/ESP32_TTGO_LoRa_Receiver.ino
@@ -0,0 +1,114 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com  
+*********/
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 866E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+String LoRaData;
+
+void setup() { 
+  
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+  
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("LORA RECEIVER ");
+  display.display();
+  
+  //initialize Serial Monitor
+  Serial.begin(115200);
+
+  Serial.println("LoRa Receiver Test");
+  
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0,10);
+  display.println("LoRa Initializing OK!");
+  display.display();  
+}
+
+void loop() {
+
+  //try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    //received a packet
+    Serial.print("Received packet ");
+
+    //read packet
+    while (LoRa.available()) {
+      LoRaData = LoRa.readString();
+      Serial.print(LoRaData);
+    }
+
+    //print RSSI of packet
+    int rssi = LoRa.packetRssi();
+    Serial.print(" with RSSI ");    
+    Serial.println(rssi);
+
+   // Dsiplay information
+   display.clearDisplay();
+   display.setCursor(0,0);
+   display.print("LORA RECEIVER");
+   display.setCursor(0,20);
+   display.print("Received packet:");
+   display.setCursor(0,30);
+   display.print(LoRaData);
+   display.setCursor(0,40);
+   display.print("RSSI:");
+   display.setCursor(30,40);
+   display.print(rssi);
+   display.display();   
+  }
+}
