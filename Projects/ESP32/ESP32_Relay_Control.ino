ca00239c110ae46371601a4585a7e6bf7764ff8c
Rui Santos <randomnerdtutorials@gmail.com>
2019-12-18 11:31:55 +0000

    Create ESP32_Relay_Control.ino

---
+++ Projects/ESP32/ESP32_Relay_Control.ino
@@ -0,0 +1,28 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-relay-module-ac-web-server/
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+const int relay = 26;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(relay, OUTPUT);
+}
+
+void loop() {
+  // Normally Open configuration, send LOW signal to let current flow
+  // (if you're usong Normally Closed configuration send HIGH signal)
+  digitalWrite(relay, LOW);
+  Serial.println("Current Flowing");
+  delay(5000); 
+  
+  // Normally Open configuration, send HIGH signal stop current flow
+  // (if you're usong Normally Closed configuration send LOW signal)
+  digitalWrite(relay, HIGH);
+  Serial.println("Current not Flowing");
+  delay(5000);
+}
