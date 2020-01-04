b18d35fe3a41adf98ba56d542a465c73faef7b88
Rui Santos <randomnerdtutorials@gmail.com>
2020-01-04 15:03:46 +0000

    Create esp_relay_web_server_boot.py

---
+++ Projects/ESP-MicroPython/esp_relay_web_server_boot.py
@@ -0,0 +1,35 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'REPLACE_WITH_YOUR_SSID'
+password = 'REPLACE_WITH_YOUR_PASSWORD'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+# ESP32 GPIO 26
+relay = Pin(26, Pin.OUT)
+
+# ESP8266 GPIO 5
+#relay = Pin(5, Pin.OUT)
