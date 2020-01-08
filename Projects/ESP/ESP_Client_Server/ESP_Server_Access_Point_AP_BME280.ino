d8f04fb839802a76af6909dc7dd6d014d702cb08
Rui Santos <randomnerdtutorials@gmail.com>
2020-01-08 12:00:13 +0000

    Update ESP_Server_Access_Point_AP_BME280.ino

--- Projects/ESP/ESP_Client_Server/ESP_Server_Access_Point_AP_BME280.ino
+++ Projects/ESP/ESP_Client_Server/ESP_Server_Access_Point_AP_BME280.ino
@@ -10,11 +10,7 @@
 */
 
 // Import required libraries
-#ifdef ESP32
-  #include "WiFi.h"
-#else
-  #include <ESP8266WiFi.h>
-#endif
+#include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 
 #include <Wire.h>
