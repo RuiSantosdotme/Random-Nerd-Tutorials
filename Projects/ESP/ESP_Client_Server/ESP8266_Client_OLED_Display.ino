4c15723677ae85616a34c81f9acc74d34c94fa90
Rui Santos <randomnerdtutorials@gmail.com>
2020-01-08 12:04:13 +0000

    Create ESP8266_Client_OLED_Display.ino

---
+++ Projects/ESP/ESP_Client_Server/ESP8266_Client_OLED_Display.ino
@@ -0,0 +1,142 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-client-server-wi-fi/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "ESP8266-Access-Point";
+const char* password = "123456789";
+
+//Your IP address or domain name with URL path
+const char* serverNameTemp = "http://192.168.4.1/temperature";
+const char* serverNameHumi = "http://192.168.4.1/humidity";
+const char* serverNamePres = "http://192.168.4.1/pressure";
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+String temperature;
+String humidity;
+String pressure;
+
+unsigned long previousMillis = 0;
+const long interval = 5000; 
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  
+  // Address 0x3C for 128x64, you might need to change this value (use an I2C scanner)
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis >= interval) {
+     // Check WiFi connection status
+    if(WiFi.status() == WL_CONNECTED){ 
+      temperature = httpGETRequest(serverNameTemp);
+      humidity = httpGETRequest(serverNameHumi);
+      pressure = httpGETRequest(serverNamePres);
+      Serial.println("Temperature: " + temperature + " *C - Humidity: " + humidity + " % - Pressure: " + pressure + " hPa");
+      
+      display.clearDisplay();
+      
+      // display temperature
+      display.setTextSize(2);
+      display.setCursor(0,0);
+      display.print("T: ");
+      display.print(temperature);
+      display.print(" ");
+      display.setTextSize(1);
+      display.cp437(true);
+      display.write(248);
+      display.setTextSize(2);
+      display.print("C");
+      
+      // display humidity
+      display.setTextSize(2);
+      display.setCursor(0, 25);
+      display.print("H: ");
+      display.print(humidity);
+      display.print(" %"); 
+      
+      // display pressure
+      display.setTextSize(2);
+      display.setCursor(0, 50);
+      display.print("P:");
+      display.print(pressure);
+      display.setTextSize(1);
+      display.setCursor(110, 56);
+      display.print("hPa");
+           
+      display.display();
+      
+      // save the last HTTP GET Request
+      previousMillis = currentMillis;
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
