1c1c593315011e7f98844eebc3ab2ccd1c841b8b
Rui Santos <randomnerdtutorials@gmail.com>
2020-01-30 13:39:18 +0000

    Create ESP_Get_MAC_Address.ino

---
+++ Projects/ESP/ESP_Get_MAC_Address.ino
@@ -0,0 +1,16 @@
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+}
+ 
+void loop(){
+
+}
