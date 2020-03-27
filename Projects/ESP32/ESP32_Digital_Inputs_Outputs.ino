8772f35139147cda5aec9924ce62eadac7dfd1f3
Rui Santos <randomnerdtutorials@gmail.com>
2020-03-27 17:35:32 +0000

    Create ESP32_Digital_Inputs_Outputs.ino

---
+++ Projects/ESP32/ESP32_Digital_Inputs_Outputs.ino
@@ -0,0 +1,31 @@
+// Complete Instructions: https://RandomNerdTutorials.com/esp32-digital-inputs-outputs-arduino/
+
+// set pin numbers
+const int buttonPin = 4;  // the number of the pushbutton pin
+const int ledPin =  5;    // the number of the LED pin
+
+// variable for storing the pushbutton status 
+int buttonState = 0;
+
+void setup() {
+  Serial.begin(115200);  
+  // initialize the pushbutton pin as an input
+  pinMode(buttonPin, INPUT);
+  // initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH
+  if (buttonState == HIGH) {
+    // turn LED on
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off
+    digitalWrite(ledPin, LOW);
+  }
+}
