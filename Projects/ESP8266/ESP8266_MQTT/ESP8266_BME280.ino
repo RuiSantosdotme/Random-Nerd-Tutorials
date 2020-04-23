cf56165fade6bc09f5545f3849abef882812cf9d
Rui Santos <randomnerdtutorials@gmail.com>
2020-04-23 16:22:36 +0100

    Update ESP8266_BME280.ino

--- Projects/ESP8266/ESP8266_MQTT/ESP8266_BME280.ino
+++ Projects/ESP8266/ESP8266_MQTT/ESP8266_BME280.ino
@@ -1,6 +1,6 @@
 /*
   Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-mqtt-publish-bme280-arduino/
+  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-mqtt-publish-bme280-arduino/
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
