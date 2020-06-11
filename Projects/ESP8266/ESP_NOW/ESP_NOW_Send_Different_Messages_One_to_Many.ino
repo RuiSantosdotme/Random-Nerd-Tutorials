646f7f8b03d9b2fc2d8e946a8dc5293b1f019193
Rui Santos <randomnerdtutorials@gmail.com>
2020-06-11 19:05:13 +0100

    Create ESP_NOW_Send_Different_Messages_One_to_Many.ino

---
+++ Projects/ESP8266/ESP_NOW/ESP_NOW_Send_Different_Messages_One_to_Many.ino
@@ -0,0 +1,84 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp8266-nodemcu/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+// REPLACE WITH RECEIVER MAC Address
+uint8_t broadcastAddress1[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t broadcastAddress2[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct test_struct {
+    int x;
+    int y;
+} test_struct;
+
+// Create a struct_message called test to store variables to be sent
+test_struct test;
+test_struct test2;
+
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  char macStr[18];
+  Serial.print("Packet to:");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+         mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+  }
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_add_peer(broadcastAddress1, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+  esp_now_add_peer(broadcastAddress2, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+
+}
+ 
+void loop() {
+  // Set values to send
+  test.x = random(1, 50);
+  test.y = random(1, 50);
+  test2.x = random(1, 50);
+  test2.y = random(1, 50);
+
+  // Send message via ESP-NOW
+  esp_now_send(broadcastAddress1, (uint8_t *) &test, sizeof(test));
+  esp_now_send(broadcastAddress2, (uint8_t *) &test2, sizeof(test2));
+
+  delay(2000);
+}
