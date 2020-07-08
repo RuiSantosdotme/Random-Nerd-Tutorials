6bc0ab6e8c3e34764a8bdf1a797f9e9e57d95158
Rui Santos <randomnerdtutorials@gmail.com>
2020-07-08 15:14:04 +0100

    Create ESP_Telegram_Request_Readings.ino

---
+++ Projects/ESP/ESP_Telegram/ESP_Telegram_Request_Readings.ino
@@ -0,0 +1,123 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/telegram-request-esp32-esp8266-nodemcu-sensor-readings/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  Project created using Brian Lough's Universal Telegram Bot Library: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+*/
+
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h> // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
+// Replace with your network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Use @myidbot to find out the chat ID of an individual or a group
+// Also note that you need to click "start" on a bot before it can
+// message you
+#define CHAT_ID "XXXXXXXXXX"
+
+// Initialize Telegram BOT
+#define BOTtoken "XXXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  // your Bot Token (Get from Botfather)
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+//Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+// BME280 connect to ESP32 I2C (GPIO 21 = SDA, GPIO 22 = SCL)
+// BME280 connect to ESP8266 I2C (GPIO 4 = SDA, GPIO 5 = SCL)
+Adafruit_BME280 bme;         
+
+// Get BME280 sensor readings and return them as a String variable
+String getReadings(){
+  float temperature, humidity;
+  temperature = bme.readTemperature();
+  humidity = bme.readHumidity();
+  String message = "Temperature: " + String(temperature) + " ºC \n";
+  message += "Humidity: " + String (humidity) + " % \n";
+  return message;
+}
+
+//Handle what happens when you receive new messages
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i=0; i<numNewMessages; i++) {
+    // Chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+    
+    // Print the received message
+    String text = bot.messages[i].text;
+    Serial.println(text);
+
+    String from_name = bot.messages[i].from_name;
+
+    if (text == "/start") {
+      String welcome = "Welcome, " + from_name + ".\n";
+      welcome += "Use the following command to get current readings.\n\n";
+      welcome += "/readings \n";
+      bot.sendMessage(chat_id, welcome, "");
+    }
+
+    if (text == "/readings") {
+      String readings = getReadings();
+      bot.sendMessage(chat_id, readings, "");
+    }  
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  #ifdef ESP8266
+    client.setInsecure();
+  #endif
+
+  // Init BME280 sensor
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if (millis() > lastTimeBotRan + botRequestDelay)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while(numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+}
