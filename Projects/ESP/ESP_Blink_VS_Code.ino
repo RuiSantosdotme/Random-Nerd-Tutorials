fa62ec1a1e2298e504c652c514aa6b0076dad1f5
Rui Santos <randomnerdtutorials@gmail.com>
2020-09-24 14:48:16 +0100

    Create ESP_Blink_VS_Code.ino

---
+++ Projects/ESP/ESP_Blink_VS_Code.ino
@@ -0,0 +1,24 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/vs-code-platformio-ide-esp32-esp8266-arduino/
+*********/
+
+#include <Arduino.h>
+
+#define LED 2
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  Serial.println("LED is on");
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("LED is off");
+  delay(1000);
+}
