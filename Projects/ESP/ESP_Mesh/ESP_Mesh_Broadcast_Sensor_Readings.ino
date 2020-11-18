2cb302422b3759c9a69aa1889575df6c86fb64f3
Rui Santos <randomnerdtutorials@gmail.com>
2020-11-18 10:48:06 +0000

    Create ESP_Mesh_Broadcast_Sensor_Readings.ino

---
+++ Projects/ESP/ESP_Mesh/ESP_Mesh_Broadcast_Sensor_Readings.ino
@@ -0,0 +1,118 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-mesh-esp32-esp8266-painlessmesh/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include "painlessMesh.h"
+#include <Arduino_JSON.h>
+
+// MESH Details
+#define   MESH_PREFIX     "RNTMESH" //name for your MESH
+#define   MESH_PASSWORD   "MESHpassword" //password for your MESH
+#define   MESH_PORT       5555 //default port
+
+//BME object on the default I2C pins
+Adafruit_BME280 bme;
+
+//Number for this node
+int nodeNumber = 2;
+
+//String to send to other nodes with sensor readings
+String readings;
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+String getReadings(); // Prototype for sending sensor readings
+
+//Create tasks: to send messages and get readings;
+Task taskSendMessage(TASK_SECOND * 5 , TASK_FOREVER, &sendMessage);
+
+String getReadings () {
+  JSONVar jsonReadings;
+  jsonReadings["node"] = nodeNumber;
+  jsonReadings["temp"] = bme.readTemperature();
+  jsonReadings["hum"] = bme.readHumidity();
+  jsonReadings["pres"] = bme.readPressure()/100.0F;
+  readings = JSON.stringify(jsonReadings);
+  return readings;
+}
+
+void sendMessage () {
+  String msg = getReadings();
+  mesh.sendBroadcast(msg);
+}
+
+//Init BME280
+void initBME(){
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }  
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+  JSONVar myObject = JSON.parse(msg.c_str());
+  int node = myObject["node"];
+  double temp = myObject["temp"];
+  double hum = myObject["hum"];
+  double pres = myObject["pres"];
+  Serial.print("Node: ");
+  Serial.println(node);
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.println(" %");
+  Serial.print("Pressure: ");
+  Serial.print(temp);
+  Serial.println(" hpa");
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  initBME();
+
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
+}
+
+void loop() {
+  // it will run the user scheduler as well
+  mesh.update();
+}
