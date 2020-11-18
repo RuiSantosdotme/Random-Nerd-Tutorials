7ff21f41a3d0cea9fad3c24128e46e4105b05a57
Rui Santos <randomnerdtutorials@gmail.com>
2020-11-18 11:16:11 +0000

    Update ESP_Mesh_Basic.ino

--- Projects/ESP/ESP_Mesh/ESP_Mesh_Basic.ino
+++ Projects/ESP/ESP_Mesh/ESP_Mesh_Basic.ino
@@ -1,7 +1,8 @@
 /*
   Rui Santos
   Complete project details at https://RandomNerdTutorials.com/esp-mesh-esp32-esp8266-painlessmesh/
-  This is a simple example that uses the painlessMesh library
+  
+  This is a simple example that uses the painlessMesh library: https://github.com/gmag11/painlessMesh/blob/master/examples/basic/basic.ino
 */
 
 #include "painlessMesh.h"
