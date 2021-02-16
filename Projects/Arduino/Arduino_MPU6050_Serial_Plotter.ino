1c9cca48349872014a59ff9d4f9ea8b8b15431a7
Rui Santos <randomnerdtutorials@gmail.com>
2021-02-16 14:52:06 +0000

    Create Arduino_MPU6050_Serial_Plotter.ino

---
+++ Projects/Arduino/Arduino_MPU6050_Serial_Plotter.ino
@@ -0,0 +1,53 @@
+// Basic demo for accelerometer readings from Adafruit MPU6050
+
+// Arduino Guide: https://RandomNerdTutorials.com/arduino-mpu-6050-accelerometer-gyroscope/
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  }
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.println("");
+  delay(100);
+}
+
+void loop() {
+
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print(a.acceleration.x);
+  Serial.print(",");
+  Serial.print(a.acceleration.y);
+  Serial.print(",");
+  Serial.print(a.acceleration.z);
+  Serial.print(", ");
+  Serial.print(g.gyro.x);
+  Serial.print(",");
+  Serial.print(g.gyro.y);
+  Serial.print(",");
+  Serial.print(g.gyro.z);
+  Serial.println("");
+
+  delay(10);
+}
