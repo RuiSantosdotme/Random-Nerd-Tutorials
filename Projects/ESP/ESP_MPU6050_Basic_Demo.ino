6df54337eba3675273b8898467a74ad59fb0c483
Rui Santos <randomnerdtutorials@gmail.com>
2021-02-16 14:55:31 +0000

    Update ESP_MPU6050_Basic_Demo.ino

--- Projects/ESP/ESP_MPU6050_Basic_Demo.ino
+++ Projects/ESP/ESP_MPU6050_Basic_Demo.ino
@@ -2,6 +2,7 @@
 
 // ESP32 Guide: https://RandomNerdTutorials.com/esp32-mpu-6050-accelerometer-gyroscope-arduino/
 // ESP8266 Guide: https://RandomNerdTutorials.com/esp8266-nodemcu-mpu-6050-accelerometer-gyroscope-arduino/
+// Arduino Guide: https://RandomNerdTutorials.com/arduino-mpu-6050-accelerometer-gyroscope/
 
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
