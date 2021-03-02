ade733de0b0d53826ff5ab0895724ca0be890065
Rui Santos <randomnerdtutorials@gmail.com>
2021-03-02 15:13:28 +0000

    Create ESP32_Save_Credentials.ino

---
+++ Projects/ESP32/Preferences/ESP32_Save_Credentials.ino
@@ -0,0 +1,34 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-save-data-permanently-preferences/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <Preferences.h>
+
+Preferences preferences;
+
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  preferences.begin("credentials", false);
+  preferences.putString("ssid", ssid); 
+  preferences.putString("password", password);
+
+  Serial.println("Network Credentials Saved using Preferences");
+
+  preferences.end();
+}
+
+void loop() {
+
+}
