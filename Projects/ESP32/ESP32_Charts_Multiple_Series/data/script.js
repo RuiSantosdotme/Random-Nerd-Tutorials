c20d83cc4c44e2c90cbefeaa8ab93b836a9bc195
Rui Santos <ee11077@fe.up.pt>
2021-04-28 11:49:24 +0100

    Add files via upload

---
+++ Projects/ESP32/ESP32_Charts_Multiple_Series/data/script.js
@@ -0,0 +1,131 @@
+// Complete project details: https://randomnerdtutorials.com/esp32-plot-readings-charts-multiple/
+
+// Get current sensor readings when the page loads
+window.addEventListener('load', getReadings);
+
+// Create Temperature Chart
+var chartT = new Highcharts.Chart({
+  chart:{
+    renderTo:'chart-temperature'
+  },
+  series: [
+    {
+      name: 'Temperature #1',
+      type: 'line',
+      color: '#101D42',
+      marker: {
+        symbol: 'circle',
+        radius: 3,
+        fillColor: '#101D42',
+      }
+    },
+    {
+      name: 'Temperature #2',
+      type: 'line',
+      color: '#00A6A6',
+      marker: {
+        symbol: 'square',
+        radius: 3,
+        fillColor: '#00A6A6',
+      }
+    },
+    {
+      name: 'Temperature #3',
+      type: 'line',
+      color: '#8B2635',
+      marker: {
+        symbol: 'triangle',
+        radius: 3,
+        fillColor: '#8B2635',
+      }
+    },
+    {
+      name: 'Temperature #4',
+      type: 'line',
+      color: '#71B48D',
+      marker: {
+        symbol: 'triangle-down',
+        radius: 3,
+        fillColor: '#71B48D',
+      }
+    },
+  ],
+  title: {
+    text: undefined
+  },
+  xAxis: {
+    type: 'datetime',
+    dateTimeLabelFormats: { second: '%H:%M:%S' }
+  },
+  yAxis: {
+    title: {
+      text: 'Temperature Celsius Degrees'
+    }
+  },
+  credits: {
+    enabled: false
+  }
+});
+
+
+//Plot temperature in the temperature chart
+function plotTemperature(jsonValue) {
+
+  var keys = Object.keys(jsonValue);
+  console.log(keys);
+  console.log(keys.length);
+
+  for (var i = 0; i < keys.length; i++){
+    var x = (new Date()).getTime();
+    console.log(x);
+    const key = keys[i];
+    var y = Number(jsonValue[key]);
+    console.log(y);
+
+    if(chartT.series[i].data.length > 40) {
+      chartT.series[i].addPoint([x, y], true, true, true);
+    } else {
+      chartT.series[i].addPoint([x, y], true, false, true);
+    }
+
+  }
+}
+
+// Function to get current readings on the webpage when it loads for the first time
+function getReadings(){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+      console.log(myObj);
+      plotTemperature(myObj);
+    }
+  };
+  xhr.open("GET", "/readings", true);
+  xhr.send();
+}
+
+if (!!window.EventSource) {
+  var source = new EventSource('/events');
+
+  source.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
+
+  source.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+
+  source.addEventListener('message', function(e) {
+    console.log("message", e.data);
+  }, false);
+
+  source.addEventListener('new_readings', function(e) {
+    console.log("new_readings", e.data);
+    var myObj = JSON.parse(e.data);
+    console.log(myObj);
+    plotTemperature(myObj);
+  }, false);
+}
