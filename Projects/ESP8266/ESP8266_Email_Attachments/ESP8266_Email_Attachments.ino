b252e697a80872b654c42b83be0493417cadb35c
Rui Santos <ee11077@fe.up.pt>
2021-06-10 11:36:51 +0100

    Add files via upload

---
+++ Projects/ESP8266/ESP8266_Email_Attachments/ESP8266_Email_Attachments.ino
@@ -0,0 +1,171 @@
+/*
+  Rui Santos
+  Complete project details at:
+   - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-send-email-smtp-server-arduino/
+   
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  Example adapted K. Suwatchai (Mobizt): https://github.com/mobizt/ESP-Mail-Client Copyright (c) 2021 mobizt
+*/
+
+//To use send Email for Gmail to port 465 (SSL), less secure app option should be enabled. https://myaccount.google.com/lesssecureapps?pli=1
+//The file systems for flash and sd memory can be changed in ESP_Mail_FS.h.
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP_Mail_Client.h>
+
+#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
+#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
+
+#define SMTP_HOST "smtp.gmail.com"
+
+/** The smtp port e.g. 
+ * 25  or esp_mail_smtp_port_25
+ * 465 or esp_mail_smtp_port_465
+ * 587 or esp_mail_smtp_port_587
+*/
+#define SMTP_PORT 465
+
+/* The log in credentials */
+#define AUTHOR_EMAIL "YOUR_EMAIL@XXXX.com"
+#define AUTHOR_PASSWORD "YOUR_EMAIL_PASS"
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "RECIPIENTE_EMAIL@XXXX.com"
+/* The SMTP Session object used for Email sending */
+SMTPSession smtp;
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status);
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting to AP");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  if (!LittleFS.begin()) {
+  // Uncomment to use SPIFFS
+  //if (!SPIFFS.begin()) {
+    Serial.println("An error has occurred while mounting the filesystem");
+  }
+  else{
+    Serial.println("Filesystem mounted successfully");
+  }
+
+  /** Enable the debug via Serial port
+   * none debug or 0
+   * basic debug or 1
+  */
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+
+  /* Declare the session config data */
+  ESP_Mail_Session session;
+
+  /* Set the session config */
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Enable the chunked data transfer with pipelining for large message if server supported */
+  message.enable.chunking = true;
+
+  /* Set the message headers */
+  message.sender.name = "ESP";
+  message.sender.email = AUTHOR_EMAIL;
+
+  message.subject = "Email with attachments";
+  message.addRecipient("Sara", RECIPIENT_EMAIL);
+
+  String htmlMsg = "This message contains attachments: image and text file.";
+  message.html.content = htmlMsg.c_str();
+  message.html.charSet = "utf-8";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_qp;
+
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_normal;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+
+  /* The attachment data item */
+  SMTP_Attachment att;
+
+  /** Set the attachment info e.g. 
+   * file name, MIME type, file path, file storage type,
+   * transfer encoding and content encoding
+  */
+  att.descr.filename = "image.png";
+  att.descr.mime = "image/png"; 
+  att.file.path = "/image.png";
+  att.file.storage_type = esp_mail_file_storage_type_flash;
+  att.descr.transfer_encoding = Content_Transfer_Encoding::enc_base64;
+
+  /* Add attachment to the message */
+  message.addAttachment(att);
+
+  message.resetAttachItem(att);
+  att.descr.filename = "text_file.txt";
+  att.descr.mime = "text/plain";
+  att.file.path = "/text_file.txt";
+  att.file.storage_type = esp_mail_file_storage_type_flash;
+  att.descr.transfer_encoding = Content_Transfer_Encoding::enc_base64;
+
+  /* Add attachment to the message */
+  message.addAttachment(att);
+
+  /* Connect to server with the session config */
+  if (!smtp.connect(&session))
+    return;
+
+  /* Start sending the Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message, true))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+}
+
+void loop(){
+}
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients);
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject);
+    }
+    Serial.println("----------------\n");
+  }
+}
