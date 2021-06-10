955e40d0799d55ef1df226b7bc557ed25549dcce
Rui Santos <randomnerdtutorials@gmail.com>
2021-06-10 10:42:34 +0100

    Update ESP_Email_Simple.ino

--- Projects/ESP/ESP_Email_Simple.ino
+++ Projects/ESP/ESP_Email_Simple.ino
@@ -2,6 +2,7 @@
   Rui Santos
   Complete project details at:
    - ESP32: https://RandomNerdTutorials.com/esp32-send-email-smtp-server-arduino-ide/
+   - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-send-email-smtp-server-arduino/
   
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
