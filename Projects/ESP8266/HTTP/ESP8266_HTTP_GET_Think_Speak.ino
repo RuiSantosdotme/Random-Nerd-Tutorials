8e2f4d5ea3c1c078cfc765530025fb8e7aa3410c
Rui Santos <randomnerdtutorials@gmail.com>
2021-06-30 17:37:30 +0100

    Update ESP8266_HTTP_GET_Think_Speak.ino

--- Projects/ESP8266/HTTP/ESP8266_HTTP_GET_Think_Speak.ino
+++ Projects/ESP8266/HTTP/ESP8266_HTTP_GET_Think_Speak.ino
@@ -3,7 +3,6 @@
   Complete project details at Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-http-get-open-weather-map-thingspeak-arduino/
 
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
   Code compatible with ESP8266 Boards Version 3.0.0 or above 
