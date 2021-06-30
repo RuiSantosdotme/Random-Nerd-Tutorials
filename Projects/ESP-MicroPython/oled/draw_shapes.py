2c65e3e84948464a1c5845266f8c89a623283745
Rui Santos <randomnerdtutorials@gmail.com>
2021-06-30 15:12:18 +0100

    Update draw_shapes.py

--- Projects/ESP-MicroPython/oled/draw_shapes.py
+++ Projects/ESP-MicroPython/oled/draw_shapes.py
@@ -1,15 +1,15 @@
 # Complete project details at https://RandomNerdTutorials.com/micropython-ssd1306-oled-scroll-shapes-esp32-esp8266/
 
-from machine import Pin, I2C
+from machine import Pin, SoftI2C
 import ssd1306
 from time import sleep
 import gfx
 
-# ESP32 Pin assignment 
-i2c = I2C(-1, scl=Pin(22), sda=Pin(21))
+# ESP32 Pin assignment
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 
 # ESP8266 Pin assignment
-#i2c = I2C(-1, scl=Pin(5), sda=Pin(4))
+#i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 
 oled_width = 128
 oled_height = 64
@@ -23,33 +23,33 @@ while True:
   oled.show()
   sleep(1)
   oled.fill(0)
-  
+
   graphics.rect(10, 10, 50, 30, 1)
   oled.show()
   sleep(1)
   oled.fill(0)
-  
+
   graphics.fill_rect(10, 10, 50, 30, 1)
   oled.show()
   sleep(1)
   oled.fill(0)
 
-  
+
   graphics.circle(64, 32, 10, 1)
   oled.show()
   sleep(1)
   oled.fill(0)
-  
+
   graphics.fill_circle(64, 32, 10, 1)
   oled.show()
   sleep(1)
   oled.fill(0)
-  
+
   graphics.triangle(10,10,55,20,5,40,1)
   oled.show()
   sleep(1)
   oled.fill(0)
-  
+
   graphics.fill_triangle(10,10,55,20,5,40,1)
   oled.show()
   sleep(1)
