2fe123846a96ca6862fe356b9551329e5b3924f1
Rui Santos <randomnerdtutorials@gmail.com>
2021-08-24 15:39:05 +0100

    Update main_basic.py

--- Projects/ESP-MicroPython/HCSR04/main_basic.py
+++ Projects/ESP-MicroPython/HCSR04/main_basic.py
@@ -3,10 +3,10 @@ from hcsr04 import HCSR04
 from time import sleep
 
 # ESP32
-sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
+sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
 
 # ESP8266
-#sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
+#sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
 
 while True:
     distance = sensor.distance_cm()
