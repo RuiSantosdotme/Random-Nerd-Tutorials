9f4d999145c5fd95881e12f51761a33351a7cc33
Rui Santos <randomnerdtutorials@gmail.com>
2021-08-25 16:24:25 +0100

    Create ESP32_Stepper_Motor_Basic.ino

---
+++ Projects/ESP32/ESP32_Stepper_Motor_Basic.ino
@@ -0,0 +1,44 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-stepper-motor-28byj-48-uln2003/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  Based on Stepper Motor Control - one revolution by Tom Igoe
+*/
+
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+
+// ULN2003 Motor Driver Pins
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 17
+
+// initialize the stepper library
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+
+void setup() {
+  // set the speed at 5 rpm:
+  myStepper.setSpeed(5);
+  // initialize the serial port:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // step one revolution in one direction:
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(1000);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(1000);
+}
