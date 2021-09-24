d67e953bf217f0ef0ad74a71e9c76811ca37d4fe
Rui Santos <randomnerdtutorials@gmail.com>
2021-09-24 14:11:58 +0100

    255

--- Projects/ESP8266/ESP8266_PWM_Fade_LED.ino
+++ Projects/ESP8266/ESP8266_PWM_Fade_LED.ino
@@ -11,14 +11,14 @@ void setup() {
 
 void loop() {
   // increase the LED brightness
-  for(int dutyCycle = 0; dutyCycle < 1023; dutyCycle++){   
+  for(int dutyCycle = 0; dutyCycle < 255; dutyCycle++){   
     // changing the LED brightness with PWM
     analogWrite(ledPin, dutyCycle);
     delay(1);
   }
 
   // decrease the LED brightness
-  for(int dutyCycle = 1023; dutyCycle > 0; dutyCycle--){
+  for(int dutyCycle = 255; dutyCycle > 0; dutyCycle--){
     // changing the LED brightness with PWM
     analogWrite(ledPin, dutyCycle);
     delay(1);
