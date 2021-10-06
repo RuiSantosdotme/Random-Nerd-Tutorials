2608beda4227f96a8b7d7aaf19015b567ed378ae
Rui Santos <randomnerdtutorials@gmail.com>
2021-10-06 16:58:17 +0100

    Create ESP32_Stepper_Motor_Web_Server.ino

---
+++ Projects/ESP32/ESP32_Stepper_Motor_Web_Server.ino
@@ -0,0 +1,132 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/stepper-motor-esp32-web-server/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Stepper.h>
+
+// Stepper Motor Settings
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 17
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+
+// Replace with your network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Search for parameters in HTTP POST request
+const char* PARAM_INPUT_1 = "direction";
+const char* PARAM_INPUT_2 = "steps";
+
+// Variables to save values from HTML form
+String direction;
+String steps;
+
+// Variable to detect whether a new request occurred
+bool newRequest = false;
+
+// HTML to build the web page
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Stepper Motor</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <h1>Stepper Motor Control</h1>
+    <form action="/" method="POST">
+      <input type="radio" name="direction" value="CW" checked>
+      <label for="CW">Clockwise</label>
+      <input type="radio" name="direction" value="CCW">
+      <label for="CW">Counterclockwise</label><br><br><br>
+      <label for="steps">Number of steps:</label>
+      <input type="number" name="steps">
+      <input type="submit" value="GO!">
+    </form>
+</body>
+</html>
+)rawliteral";
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+
+void setup() {
+  Serial.begin(115200);
+
+  initWiFi();
+
+  myStepper.setSpeed(5);
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", index_html);
+  });
+  
+  // Handle request (form)
+  server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+    int params = request->params();
+    for(int i=0;i<params;i++){
+      AsyncWebParameter* p = request->getParam(i);
+      if(p->isPost()){
+        // HTTP POST input1 value (direction)
+        if (p->name() == PARAM_INPUT_1) {
+          direction = p->value().c_str();
+          Serial.print("Direction set to: ");
+          Serial.println(direction);
+        }
+        // HTTP POST input2 value (steps)
+        if (p->name() == PARAM_INPUT_2) {
+          steps = p->value().c_str();
+          Serial.print("Number of steps set to: ");
+          Serial.println(steps);
+        }
+      }
+    }
+    request->send(200, "text/html", index_html);
+    newRequest = true;
+  });
+
+  server.begin();
+}
+
+void loop() {
+  // Check if there was a new request and move the stepper accordingly
+  if (newRequest){
+    if (direction == "CW"){
+      // Spin the stepper clockwise direction
+      myStepper.step(steps.toInt());
+    }
+    else{
+      // Spin the stepper counterclockwise direction
+      myStepper.step(-steps.toInt());
+    }
+    newRequest = false;
+  }
+}
