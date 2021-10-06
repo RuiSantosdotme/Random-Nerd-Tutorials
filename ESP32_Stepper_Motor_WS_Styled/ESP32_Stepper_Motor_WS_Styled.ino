fb1336004500de7530dac244ee31061c63e84ed9
Rui Santos <ee11077@fe.up.pt>
2021-10-06 17:02:06 +0100

    Add files via upload

---
+++ ESP32_Stepper_Motor_WS_Styled/ESP32_Stepper_Motor_WS_Styled.ino
@@ -0,0 +1,121 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/stepper-motor-esp32-web-server/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+#include <Arduino_JSON.h>
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 17
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+
+// Replace with your network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Search for parameter in HTTP POST request
+const char* PARAM_INPUT_1 = "direction";
+const char* PARAM_INPUT_2 = "steps";
+
+//Variables to save values from HTML form
+String direction;
+String steps;
+
+bool newRequest = false;
+
+// Initialize SPIFFS
+void initSPIFFS() {
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  else {
+  Serial.println("SPIFFS mounted successfully");
+  }
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  // Serial port for debugging purposes
+
+  Serial.begin(115200);
+  initWiFi();
+  initSPIFFS();
+  myStepper.setSpeed(5);
+
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+  
+  server.serveStatic("/", SPIFFS, "/");
+
+  server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+    int params = request->params();
+    for(int i=0;i<params;i++){
+      AsyncWebParameter* p = request->getParam(i);
+      if(p->isPost()){
+        // HTTP POST input1 value
+        if (p->name() == PARAM_INPUT_1) {
+          direction = p->value().c_str();
+          Serial.print("Direction set to: ");
+          Serial.println(direction);
+        }
+        // HTTP POST input2 value
+        if (p->name() == PARAM_INPUT_2) {
+          steps = p->value().c_str();
+          Serial.print("Number of steps set to: ");
+          Serial.println(steps);
+          // Write file to save value
+        }
+        newRequest = true;
+        //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+      }
+    }
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.begin();
+}
+
+void loop() {
+  if (newRequest){
+    if (direction == "CW"){
+      myStepper.step(steps.toInt());
+      Serial.print("CW");
+    }
+    else{
+      myStepper.step(-steps.toInt());
+    }
+    newRequest = false;
+  }
+}
