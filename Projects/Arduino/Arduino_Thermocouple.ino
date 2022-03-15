58a3cd94964445a7406861b20e4903cdbb5009b5
Rui Santos <randomnerdtutorials@gmail.com>
2022-03-15 16:10:24 +0000

    Create Arduino_Thermocouple.ino

---
+++ Projects/Arduino/Arduino_Thermocouple.ino
@@ -0,0 +1,29 @@
+// this example is public domain. enjoy! https://learn.adafruit.com/thermocouple/
+
+#include "max6675.h"
+
+int thermoDO = 4;
+int thermoCS = 5;
+int thermoCLK = 6;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("MAX6675 test");
+  // wait for MAX chip to stabilize
+  delay(500);
+}
+
+void loop() {
+  // basic readout test, just print the current temp
+  
+  Serial.print("C = "); 
+  Serial.println(thermocouple.readCelsius());
+  Serial.print("F = ");
+  Serial.println(thermocouple.readFahrenheit());
+ 
+  // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
+  delay(1000);
+}
