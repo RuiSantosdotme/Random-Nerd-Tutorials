f1fca0bb8a69390fa32cff47cea38b547ae1f15d
Rui Santos <randomnerdtutorials@gmail.com>
2022-10-04 14:46:08 +0100

    .c_str()

--- Projects/ESP/ESP_Email_Simple.ino
+++ Projects/ESP/ESP_Email_Simple.ino
@@ -136,8 +136,8 @@ void smtpCallback(SMTP_Status status){
       ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
       ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
       ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
-      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients);
-      ESP_MAIL_PRINTF("Subject: %s\n", result.subject);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
     }
     Serial.println("----------------\n");
   }
