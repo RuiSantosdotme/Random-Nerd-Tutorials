684c2e388849752d09ab6ce66041e4583a8ab789
Rui Santos <ee11077@fe.up.pt>
2022-12-15 14:49:17 +0000

    Add files via upload

---
+++ Projects/ESP32/ESP32_HTTPS/ESP32_HTTPSClient_No_Certificate/ESP32_HTTPSClient_No_Certificate.ino
@@ -0,0 +1,66 @@
+/*
+  Complete project details: https://RandomNerdTutorials.com/
+  Based on the BasicHTTPSClient.ino example found at Examples > BasicHttpsClient
+*/
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+
+// Replace with your network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  // Initialize Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+ WiFiClientSecure *client = new WiFiClientSecure;
+  if(client) {
+    // set secure client with certificate
+    client->setInsecure();
+    //create an HTTPClient instance
+    HTTPClient https;
+
+    //Initializing an HTTPS communication using the secure client
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, "https://www.howsmyssl.com/a/check")) {  // HTTPS
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+       Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+      // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          // print server response payload
+          String payload = https.getString();
+          Serial.println(payload);
+        }
+      }
+      else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+      https.end();
+    }
+  }
+  else {
+    Serial.printf("[HTTPS] Unable to connect\n");
+  }
+  Serial.println();
+  Serial.println("Waiting 2min before the next round...");
+  delay(120000);
+}
