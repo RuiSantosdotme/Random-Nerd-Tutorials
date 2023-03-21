8a06e29223db20d112fab9b7d0de5ad8e189caa5
Rui Santos <randomnerdtutorials@gmail.com>
2023-03-21 15:45:03 +0000

    Update ESP8266_NodeRED_MQTT_LED_DHT.ino

--- Projects/ESP8266_NodeRED_MQTT_LED_DHT.ino
+++ Projects/ESP8266_NodeRED_MQTT_LED_DHT.ino
@@ -18,8 +18,14 @@
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
+// MQTT broker credentials (set to NULL if not required)
+const char* MQTT_username = "REPLACE_WITH_MQTT_USERNAME"; 
+const char* MQTT_password = "REPLACE_WITH_MQTT_PASSWORD"; 
+
 // Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
-const char* mqtt_server = "REPLACE_WITH_YOUR_RPI_IP_ADDRESS";
+const char* mqtt_server = "YOUR_BROKER_IP_ADDRESS";
+//For example
+//const char* mqtt_server = "192.168.1.106";
 
 // Initializes the espClient. You should change the espClient name if you have multiple ESPs running in your home automation system
 WiFiClient espClient;
@@ -38,7 +44,7 @@ DHT dht(DHTPin, DHTTYPE);
 long now = millis();
 long lastMeasure = 0;
 
-// Don't change the function below. This functions connects your ESP8266 to your router
+// This functions connects your ESP8266 to your router
 void setup_wifi() {
   delay(10);
   // We start by connecting to a WiFi network
@@ -55,7 +61,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-// This functions is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// This function is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
 // Change the function below to add logic to your program, so when a device publishes a message to a topic that 
 // your ESP8266 is subscribed you can actually do something
 void callback(String topic, byte* message, unsigned int length) {
@@ -105,7 +111,7 @@ void reconnect() {
        if (client.connect("ESP2_Garage")) {
       That should solve your MQTT multiple connections problem
     */
-    if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client", MQTT_username, MQTT_password)) {
       Serial.println("connected");  
       // Subscribe or resubscribe to a topic
       // You can subscribe to more topics (to control more LEDs in this example)
@@ -142,52 +148,38 @@ void loop() {
     reconnect();
   }
   if(!client.loop())
-    client.connect("ESP8266Client");
+    client.connect("ESP8266Client", MQTT_username, MQTT_password);
 
   now = millis();
   // Publishes new temperature and humidity every 30 seconds
   if (now - lastMeasure > 30000) {
     lastMeasure = now;
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
+    float humidity = dht.readHumidity();
     // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
+    float temperatureC = dht.readTemperature();
     // Read temperature as Fahrenheit (isFahrenheit = true)
-    float f = dht.readTemperature(true);
+    float temperatureF = dht.readTemperature(true);
 
     // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t) || isnan(f)) {
+    if (isnan(humidity) || isnan(temperatureC) || isnan(temperatureF)) {
       Serial.println("Failed to read from DHT sensor!");
       return;
     }
 
-    // Computes temperature values in Celsius
-    float hic = dht.computeHeatIndex(t, h, false);
-    static char temperatureTemp[7];
-    dtostrf(hic, 6, 2, temperatureTemp);
-    
-    // Uncomment to compute temperature values in Fahrenheit 
-    // float hif = dht.computeHeatIndex(f, h);
-    // static char temperatureTemp[7];
-    // dtostrf(hif, 6, 2, temperatureTemp);
-    
-    static char humidityTemp[7];
-    dtostrf(h, 6, 2, humidityTemp);
-
     // Publishes Temperature and Humidity values
-    client.publish("room/temperature", temperatureTemp);
-    client.publish("room/humidity", humidityTemp);
+    client.publish("room/temperature", String(temperatureC).c_str());
+    client.publish("room/humidity", String(humidity).c_str());
+    //Uncomment to publish temperature in F degrees
+    //client.publish("room/temperature", String(temperatureF).c_str());
     
     Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t Temperature: ");
-    Serial.print(t);
-    Serial.print(" *C ");
-    Serial.print(f);
-    Serial.print(" *F\t Heat index: ");
-    Serial.print(hic);
-    Serial.println(" *C ");
-    // Serial.print(hif);
-    // Serial.println(" *F");
+    Serial.print(humidity);
+    Serial.println(" %");
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.println(" ºC");
+    Serial.print(temperatureF);
+    Serial.println(" ºF");
   }
 } 
