8b91bc51e470d1efcbfb72604f8d80c7377aaf88
Rui Santos <randomnerdtutorials@gmail.com>
2023-04-27 15:34:24 +0100

    Create blinking_led.py

---
+++ Projects/Raspberry-Pi/blinking_led.py
@@ -0,0 +1,15 @@
+# Complete Project Details: https://randomnerdtutorials.com/projects-raspberry-pi/
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(14)
+
+# blinking an LED forever
+while True:
+  #set the led ON for one second
+  led.on()
+  sleep(1)
+  #set the led ON for one second
+  led.off()
+  sleep(1)
