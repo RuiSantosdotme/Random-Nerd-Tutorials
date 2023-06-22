97c51cec53a74599eea5b18fc58e4c6fb83b8b98
Rui Santos <randomnerdtutorials@gmail.com>
2023-06-22 18:56:26 +0100

    Update ESP_Email_Simple.ino

--- Projects/ESP/ESP_Email_Simple.ino
+++ Projects/ESP/ESP_Email_Simple.ino
@@ -9,8 +9,6 @@
   Example adapted from: https://github.com/mobizt/ESP-Mail-Client
 */
 
-// To send Emails using Gmail on port 465 (SSL), you need to create an app password: https://support.google.com/accounts/answer/185833
-
 #include <Arduino.h>
 #if defined(ESP32)
   #include <WiFi.h>
@@ -22,17 +20,18 @@
 #define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
 #define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
 
+/** The smtp host name e.g. smtp.gmail.com for GMail or smtp.office365.com for Outlook or smtp.mail.yahoo.com */
 #define SMTP_HOST "smtp.gmail.com"
 #define SMTP_PORT 465
 
 /* The sign in credentials */
 #define AUTHOR_EMAIL "YOUR_EMAIL@XXXX.com"
-#define AUTHOR_PASSWORD "YOUR_EMAIL_PASS"
+#define AUTHOR_PASSWORD "YOUR_EMAIL_APP_PASS"
 
 /* Recipient's email*/
 #define RECIPIENT_EMAIL "RECIPIENTE_EMAIL@XXXX.com"
 
-/* The SMTP Session object used for Email sending */
+/* Declare the global used SMTPSession object for SMTP transport */
 SMTPSession smtp;
 
 /* Callback function to get the Email sending status */
@@ -41,54 +40,69 @@ void smtpCallback(SMTP_Status status);
 void setup(){
   Serial.begin(115200);
   Serial.println();
-  Serial.print("Connecting to AP");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
     Serial.print(".");
-    delay(200);
+    delay(300);
   }
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
+  Serial.println();
+  Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
 
+  /*  Set the network reconnection option */
+  MailClient.networkReconnect(true);
+
   /** Enable the debug via Serial port
-   * none debug or 0
-   * basic debug or 1
-  */
+   * 0 for no debugging
+   * 1 for basic level debugging
+   *
+   * Debug port can be changed via ESP_MAIL_DEFAULT_DEBUG_PORT in ESP_Mail_FS.h
+   */
   smtp.debug(1);
 
   /* Set the callback function to get the sending results */
   smtp.callback(smtpCallback);
 
-  /* Declare the session config data */
-  ESP_Mail_Session session;
+  /* Declare the Session_Config for user defined session credentials */
+  Session_Config config;
 
   /* Set the session config */
-  session.server.host_name = SMTP_HOST;
-  session.server.port = SMTP_PORT;
-  session.login.email = AUTHOR_EMAIL;
-  session.login.password = AUTHOR_PASSWORD;
-  session.login.user_domain = "";
+  config.server.host_name = SMTP_HOST;
+  config.server.port = SMTP_PORT;
+  config.login.email = AUTHOR_EMAIL;
+  config.login.password = AUTHOR_PASSWORD;
+  config.login.user_domain = "";
+
+  /*
+  Set the NTP config time
+  For times east of the Prime Meridian use 0-12
+  For times west of the Prime Meridian add 12 to the offset.
+  Ex. American/Denver GMT would be -6. 6 + 12 = 18
+  See https://en.wikipedia.org/wiki/Time_zone for a list of the GMT/UTC timezone offsets
+  */
+  config.time.ntp_server = F("pool.ntp.org,time.nist.gov");
+  config.time.gmt_offset = 3;
+  config.time.day_light_offset = 0;
 
   /* Declare the message class */
   SMTP_Message message;
 
   /* Set the message headers */
-  message.sender.name = "ESP";
+  message.sender.name = F("ESP");
   message.sender.email = AUTHOR_EMAIL;
-  message.subject = "ESP Test Email";
-  message.addRecipient("Sara", RECIPIENT_EMAIL);
-
+  message.subject = F("ESP Test Email");
+  message.addRecipient(F("Sara"), RECIPIENT_EMAIL);
+    
   /*Send HTML message*/
-  String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Hello World!</h1><p>- Sent from ESP board</p></div>";
+  /*String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Hello World!</h1><p>- Sent from ESP board</p></div>";
   message.html.content = htmlMsg.c_str();
   message.html.content = htmlMsg.c_str();
   message.text.charSet = "us-ascii";
-  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;*/
 
-  /*
+   
   //Send raw text message
   String textMsg = "Hello World! - Sent from ESP board";
   message.text.content = textMsg.c_str();
@@ -96,22 +110,32 @@ void setup(){
   message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
   
   message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_low;
-  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;*/
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
 
-  /* Set the custom message header */
-  //message.addHeader("Message-ID: <abcde.fghij@gmail.com>");
 
-  /* Connect to server with the session config */
-  if (!smtp.connect(&session))
+  /* Connect to the server */
+  if (!smtp.connect(&config)){
+    ESP_MAIL_PRINTF("Connection error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
     return;
+  }
+
+  if (!smtp.isLoggedIn()){
+    Serial.println("\nNot yet logged in.");
+  }
+  else{
+    if (smtp.isAuthenticated())
+      Serial.println("\nSuccessfully logged in.");
+    else
+      Serial.println("\nConnected with no Auth.");
+  }
 
   /* Start sending Email and close the session */
   if (!MailClient.sendMail(&smtp, &message))
-    Serial.println("Error sending Email, " + smtp.errorReason());
+    ESP_MAIL_PRINTF("Error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
+
 }
 
 void loop(){
-
 }
 
 /* Callback function to get the Email sending status */
@@ -121,24 +145,34 @@ void smtpCallback(SMTP_Status status){
 
   /* Print the sending result */
   if (status.success()){
+    // ESP_MAIL_PRINTF used in the examples is for format printing via debug Serial port
+    // that works for all supported Arduino platform SDKs e.g. AVR, SAMD, ESP32 and ESP8266.
+    // In ESP8266 and ESP32, you can use Serial.printf directly.
+
     Serial.println("----------------");
     ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
-    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    ESP_MAIL_PRINTF("Message sent failed: %d\n", status.failedCount());
     Serial.println("----------------\n");
-    struct tm dt;
 
-    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++)
+    {
       /* Get the result item */
       SMTP_Result result = smtp.sendingResult.getItem(i);
-      time_t ts = (time_t)result.timestamp;
-      localtime_r(&ts, &dt);
 
+      // In case, ESP32, ESP8266 and SAMD device, the timestamp get from result.timestamp should be valid if
+      // your device time was synched with NTP server.
+      // Other devices may show invalid timestamp as the device time was not set i.e. it will show Jan 1, 1970.
+      // You can call smtp.setSystemTime(xxx) to set device time manually. Where xxx is timestamp (seconds since Jan 1, 1970)
+      
       ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
       ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
-      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Date/Time: %s\n", MailClient.Time.getDateTimeString(result.timestamp, "%B %d, %Y %H:%M:%S").c_str());
       ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
       ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
     }
     Serial.println("----------------\n");
+
+    // You need to clear sending result as the memory usage will grow up.
+    smtp.sendingResult.clear();
   }
 }
