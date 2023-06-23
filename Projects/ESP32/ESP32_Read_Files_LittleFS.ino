b7647265497255e8ad18095c056b5cb8223f73d9
Rui Santos <ee11077@fe.up.pt>
2023-06-23 11:36:41 +0100

    Add files via upload

---
+++ Projects/ESP32/ESP32_Read_Files_LittleFS.ino
@@ -0,0 +1,31 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include "LittleFS.h"
+ 
+void setup() {
+  Serial.begin(115200);
+  
+  if(!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  
+  File file = LittleFS.open("/test_example.txt");
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+  
+  Serial.println("File Content:");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
+}
+ 
+void loop() {
+
+}
