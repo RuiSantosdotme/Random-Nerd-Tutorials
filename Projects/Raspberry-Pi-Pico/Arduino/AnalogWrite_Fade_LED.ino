b382da6a0e9cecbf71805e8718f519a9283a5176
Rui Santos <randomnerdtutorials@gmail.com>
2023-08-22 19:08:19 +0100

    Update AnalogWrite_Fade_LED.ino

--- Projects/Raspberry-Pi-Pico/Arduino/AnalogWrite_Fade_LED.ino
+++ Projects/Raspberry-Pi-Pico/Arduino/AnalogWrite_Fade_LED.ino
@@ -1,6 +1,6 @@
 /*********
   Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-analogwrite-arduino/
+  Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-pwm-analogwrite-arduino/
   
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
