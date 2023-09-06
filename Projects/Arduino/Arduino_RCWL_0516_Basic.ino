44e3c4fffe406f2f44d3c1b63c15022426d90321
Rui Santos <randomnerdtutorials@gmail.com>
2023-09-06 17:30:38 +0100

    Create Arduino_RCWL_0516_Basic.ino

---
+++ Projects/Arduino/Arduino_RCWL_0516_Basic.ino
@@ -0,0 +1,40 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/arduino-rcwl-0516/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+//If you're using the built-in LED for testing, remember that it works with inverted logic
+//(HIGH=LOW, and LOW=HIGH)
+int led = 13;                // the pin that the LED is atteched to
+int sensor = 12;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
+
+void setup() {
+  pinMode(led, OUTPUT);      // initalize LED as an output
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(9600);        // initialize serial
+}
+
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(led, HIGH);   // turn LED ON
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       // update variable state to HIGH
+    }
+  } 
+  else {
+      digitalWrite(led, LOW); // turn LED OFF
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
+  }
+}
