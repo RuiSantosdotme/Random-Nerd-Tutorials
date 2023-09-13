df0039924d3fddb9d77d721de1c6b48bee58dd74
Rui Santos <randomnerdtutorials@gmail.com>
2023-09-13 22:35:33 +0100

    Create main_DS18B20_Single.py

---
+++ Projects/Raspberry-Pi-Pico/MicroPython/main_DS18B20_Single.py
@@ -0,0 +1,21 @@
+# Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-ds18b20-micropython/
+
+import machine, onewire, ds18x20, time
+
+ds_pin = machine.Pin(22)
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+roms = ds_sensor.scan()
+print('Found DS devices: ', roms)
+
+while True:
+  ds_sensor.convert_temp()
+  time.sleep_ms(750)
+  for rom in roms:
+    print(rom)
+    tempC = ds_sensor.read_temp(rom)
+    tempF = tempC * (9/5) +32
+    print('temperature (ºC):', "{:.2f}".format(tempC))
+    print('temperature (ºF):', "{:.2f}".format(tempF))
+    print()
+  time.sleep(5)
