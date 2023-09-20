c79977ef12434ec825e28194db6a7ead8179515c
Rui Santos <randomnerdtutorials@gmail.com>
2023-09-20 16:36:52 +0100

    Create ESP32_Write_File_LittleFS_Prevent_Overwrite.ino

---
+++ Projects/ESP32/ESP32_Write_File_LittleFS_Prevent_Overwrite.ino
@@ -0,0 +1,93 @@
+// Project details: https://RandomNerdTutorials.com/esp32-write-data-littlefs-arduino/
+
+#include <Arduino.h>
+#include "FS.h"
+#include <LittleFS.h>
+
+/* You only need to format LittleFS the first time you run a
+   test or else use the LITTLEFS plugin to create a partition
+   https://github.com/lorol/arduino-esp32littlefs-plugin
+ */
+#define FORMAT_LITTLEFS_IF_FAILED true
+
+int mydata;
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- file written");
+    } else {
+        Serial.println("- write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("- failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- message appended");
+    } else {
+        Serial.println("- append failed");
+    }
+    file.close();
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\r\n", path);
+
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("- failed to open file for reading");
+        return;
+    }
+
+    Serial.println("- read from file:");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  if(!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED)){
+      Serial.println("LittleFS Mount Failed");
+      return;
+   }
+   else{
+       Serial.println("Little FS Mounted Successfully");
+   }
+
+   // Check if the file already exists to prevent overwritting existing data
+   bool fileexists = LittleFS.exists("/data.txt");
+   Serial.print(fileexists);
+   if(!fileexists) {
+       Serial.println("File doesn’t exist");
+       Serial.println("Creating file...");
+       // Create File and add header
+       writeFile(LittleFS, "/data.txt", "MY ESP32 DATA \r\n");
+   }
+   else {
+       Serial.println("File already exists");
+   }
+}
+
+void loop() {
+  mydata = random (0, 1000);
+  appendFile(LittleFS, "/data.txt", (String(mydata)+ "\r\n").c_str()); //Append data to the file
+  readFile(LittleFS, "/data.txt"); // Read the contents of the file
+  delay(30000);
+}
