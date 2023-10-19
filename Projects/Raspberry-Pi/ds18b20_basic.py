afbeecd21f720029d90d92a530d1a002d2765215
Rui Santos <randomnerdtutorials@gmail.com>
2023-10-19 15:13:42 +0100

    Create ds18b20_basic.py

---
+++ Projects/Raspberry-Pi/ds18b20_basic.py
@@ -0,0 +1,35 @@
+# Complete Project Details: https://RandomNerdTutorials.com/raspberry-pi-ds18b20-python/
+
+# Based on the Adafruit example: https://github.com/adafruit/Adafruit_Learning_System_Guides/blob/main/Raspberry_Pi_DS18B20_Temperature_Sensing/code.py
+
+import os
+import glob
+import time
+ 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+while True:
+	print(read_temp())	
+	time.sleep(1)
