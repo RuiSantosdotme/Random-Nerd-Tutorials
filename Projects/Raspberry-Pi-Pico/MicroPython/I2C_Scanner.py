ec7c1aaf035704a1666e14827da0a451d9f28591
Rui Santos <randomnerdtutorials@gmail.com>
2023-12-14 12:48:09 +0000

    Create I2C_Scanner.py

---
+++ Projects/Raspberry-Pi-Pico/MicroPython/I2C_Scanner.py
@@ -0,0 +1,16 @@
+# I2C Scanner MicroPython
+from machine import Pin, SoftI2C
+
+# You can choose any other combination of I2C pins
+i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
+
+print('I2C SCANNER')
+devices = i2c.scan()
+
+if len(devices) == 0:
+  print("No i2c device !")
+else:
+  print('i2c devices found:', len(devices))
+
+  for device in devices:
+    print("I2C hexadecima address: ", hex(device))
