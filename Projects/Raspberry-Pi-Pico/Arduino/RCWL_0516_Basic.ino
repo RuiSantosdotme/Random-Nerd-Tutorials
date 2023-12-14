dfde3bce92175dc6710737658eaaae381139d854
Rui Santos <randomnerdtutorials@gmail.com>
2023-12-14 17:35:26 +0000

    Create RCWL_0516_Basic.ino

---
+++ Projects/Raspberry-Pi-Pico/Arduino/RCWL_0516_Basic.ino
@@ -0,0 +1,38 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-rcwl-0516-arduino/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+
+int led = 21;                // the pin that the LED is atteched to
+int sensor = 22;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
+
+void setup() {
+  pinMode(led, OUTPUT);      // initalize LED as an output
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(115200);        // initialize serial
+}
+
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(led, HIGH);   // turn LED ON
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       // update variable state to HIGH
+    }
+  } 
+  else {
+      digitalWrite(led, LOW); // turn LED OFF
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
+  }
+}
