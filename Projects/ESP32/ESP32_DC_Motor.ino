35a70daa9fc67da19e547e44b966f07a9f215b13
Rui Santos <randomnerdtutorials@gmail.com>
2024-02-22 11:39:01 +0000

    Update ESP32_DC_Motor.ino

--- Projects/ESP32/ESP32_DC_Motor.ino
+++ Projects/ESP32/ESP32_DC_Motor.ino
@@ -1,6 +1,6 @@
 /*********
   Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
+  Complete project details at https://RandomNerdTutorials.com/esp32-dc-motor-l298n-motor-driver-control-speed-direction/
 *********/
 
 // Motor A
