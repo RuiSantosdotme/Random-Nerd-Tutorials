44e68c002ba3e3d1f62ce814580b97814041f781
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-11 09:55:25 +0100

    Update ESP32_Set_Hostname.ino

--- Projects/ESP32/ESP32_Set_Hostname.ino
+++ Projects/ESP32/ESP32_Set_Hostname.ino
@@ -1,41 +1,40 @@
 /*
-  Rui Santos
+  Rui Santos & Sara Santos - Random Nerd Tutorials
   Complete project details at https://RandomNerdTutorials.com/esp32-set-custom-hostname-arduino/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-
 #include <WiFi.h>
 
-// Replace with your network credentials (STATION)
+// Replace with your network credentials
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
-
-String hostname = "ESP32 Node Temperature";
+// Change the hostname
+const char* hostname = "esp32-node-temperature";
 
 void initWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
-  WiFi.setHostname(hostname.c_str()); //define hostname
-  //wifi_station_set_hostname( hostname.c_str() );
+  WiFi.setHostname(hostname);
+
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
+  Serial.print("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(1000);
   }
+  
+  Serial.print("\nESP32 IP Address: ");
   Serial.println(WiFi.localIP());
+  Serial.print("ESP32 HostName: ");
+  Serial.println(WiFi.getHostname());
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
 }
 
 void setup() {
   Serial.begin(115200);
   initWiFi();
-  Serial.print("RRSI: ");
-  Serial.println(WiFi.RSSI());
 }
 
 void loop() {
