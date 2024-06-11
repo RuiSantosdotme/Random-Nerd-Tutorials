c4523d795f76f053ad595840337610fd1f7ea09e
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-11 10:12:41 +0100

    Update ESP8266_Get_MAC_Address.ino

--- Projects/ESP8266/ESP8266_Get_MAC_Address.ino
+++ Projects/ESP8266/ESP8266_Get_MAC_Address.ino
@@ -1,11 +1,15 @@
-// Complete Instructions to Get and Change ESP MAC Address: https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
-
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
 #include <ESP8266WiFi.h>
 
 void setup(){
   Serial.begin(115200);
   Serial.println();
-  Serial.print("ESP8266 Board MAC Address:  ");
+  Serial.print("ESP Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
 }
  
