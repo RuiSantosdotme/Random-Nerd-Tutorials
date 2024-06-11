cce56dd4835ece36e2671e49cc7d33f0ba7ae7d6
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-11 13:00:32 +0100

    Update ESP_Get_MAC_Address.ino

--- Projects/ESP/ESP_Get_MAC_Address.ino
+++ Projects/ESP/ESP_Get_MAC_Address.ino
@@ -4,13 +4,36 @@
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-#include <ESP8266WiFi.h>
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <esp_wifi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+
+void esp32ReadMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
 
 void setup(){
   Serial.begin(115200);
-  Serial.println();
-  Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
+
+  Serial.print("ESP Board MAC Address: ");
+  #ifdef ESP32
+    WiFi.mode(WIFI_STA);
+    WiFi.STA.begin();
+    esp32ReadMacAddress();
+  #else
+    Serial.println(WiFi.macAddress());
+  #endif
 }
  
 void loop(){
