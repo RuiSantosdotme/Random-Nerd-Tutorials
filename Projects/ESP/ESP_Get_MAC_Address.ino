1346add3af19fb6a1e8be40765860dc85c44831c
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-11 13:03:22 +0100

    Update ESP_Get_MAC_Address.ino

--- Projects/ESP/ESP_Get_MAC_Address.ino
+++ Projects/ESP/ESP_Get_MAC_Address.ino
@@ -11,18 +11,6 @@
   #include <ESP8266WiFi.h>
 #endif
 
-void esp32ReadMacAddress(){
-  uint8_t baseMac[6];
-  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
-  if (ret == ESP_OK) {
-    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
-                  baseMac[0], baseMac[1], baseMac[2],
-                  baseMac[3], baseMac[4], baseMac[5]);
-  } else {
-    Serial.println("Failed to read MAC address");
-  }
-}
-
 void setup(){
   Serial.begin(115200);
 
@@ -30,7 +18,15 @@ void setup(){
   #ifdef ESP32
     WiFi.mode(WIFI_STA);
     WiFi.STA.begin();
-    esp32ReadMacAddress();
+    uint8_t baseMac[6];
+    esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+    if (ret == ESP_OK) {
+      Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                    baseMac[0], baseMac[1], baseMac[2],
+                    baseMac[3], baseMac[4], baseMac[5]);
+    } else {
+      Serial.println("Failed to read MAC address");
+    }
   #else
     Serial.println(WiFi.macAddress());
   #endif
