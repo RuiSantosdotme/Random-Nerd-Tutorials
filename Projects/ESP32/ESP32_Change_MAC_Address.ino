5f5ea202020edb455f55e9c8de8588cf8c414401
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-11 10:11:28 +0100

    Update ESP32_Change_MAC_Address.ino

--- Projects/ESP32/ESP32_Change_MAC_Address.ino
+++ Projects/ESP32/ESP32_Change_MAC_Address.ino
@@ -1,28 +1,58 @@
-// Complete Instructions: https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
-
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
 #include <WiFi.h>
 #include <esp_wifi.h>
 
 // Set your new MAC Address
 uint8_t newMACAddress[] = {0x32, 0xAE, 0xA4, 0x07, 0x0D, 0x66};
 
+// Replace with your network credentials
+const char* ssid     = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
+
 void setup(){
   Serial.begin(115200);
   Serial.println();
   
   WiFi.mode(WIFI_STA);
+  WiFi.STA.begin();
   
-  Serial.print("[OLD] ESP32 Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
-  
-  // ESP32 Board add-on before version < 1.0.5
-  //esp_wifi_set_mac(ESP_IF_WIFI_STA, &newMACAddress[0]);
-  
-  // ESP32 Board add-on after version > 1.0.5
-  esp_wifi_set_mac(WIFI_IF_STA, &newMACAddress[0]);
-  
-  Serial.print("[NEW] ESP32 Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
+  Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
+  readMacAddress();
+
+  esp_err_t err = esp_wifi_set_mac(WIFI_IF_STA, &newMACAddress[0]);
+  if (err == ESP_OK) {
+    Serial.println("Success changing Mac Address");
+  }
+
+  // Read the new MAC address
+  Serial.print("[NEW] ESP32 Board MAC Address: ");
+  readMacAddress();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Connected");
 }
  
 void loop(){
