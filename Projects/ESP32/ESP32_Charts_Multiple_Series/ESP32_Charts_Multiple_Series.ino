5924ff58a0e1a68ce169f12b03eeadedc50d2ab6
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-14 10:52:35 +0100

    Update ESP32_Charts_Multiple_Series.ino

--- Projects/ESP32/ESP32_Charts_Multiple_Series/ESP32_Charts_Multiple_Series.ino
+++ Projects/ESP32/ESP32_Charts_Multiple_Series/ESP32_Charts_Multiple_Series.ino
@@ -1,16 +1,14 @@
 /*********
-  Rui Santos
+  Rui Santos & Sara Santos - Random Nerd Tutorials
   Complete instructions at https://RandomNerdTutorials.com/esp32-plot-readings-charts-multiple/
-
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 *********/
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include "SPIFFS.h"
+#include "LittleFS.h"
 #include <Arduino_JSON.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -59,13 +57,13 @@ String getSensorReadings(){
   return jsonString;
 }
 
-// Initialize SPIFFS
-void initSPIFFS() {
-  if (!SPIFFS.begin()) {
-    Serial.println("An error has occurred while mounting SPIFFS");
+// Initialize LittleFS
+void initLittleFS() {
+  if (!LittleFS.begin()) {
+    Serial.println("An error has occurred while mounting LittleFS");
   }
   else{
-    Serial.println("SPIFFS mounted successfully");
+    Serial.println("LittleFS mounted successfully");
   }
 }
 
@@ -85,14 +83,14 @@ void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
   initWiFi();
-  initSPIFFS();
+  initLittleFS();
 
   // Web Server Root URL
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
+    request->send(LittleFS, "/index.html", "text/html");
   });
 
-  server.serveStatic("/", SPIFFS, "/");
+  server.serveStatic("/", LittleFS, "/");
 
   // Request for the latest sensor readings
   server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request){
