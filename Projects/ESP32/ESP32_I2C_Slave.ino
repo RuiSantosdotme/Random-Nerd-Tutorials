c1842ee45e0ec4bca487177e7c33ca452c7be79e
Rui Santos <randomnerdtutorials@gmail.com>
2024-06-20 11:56:36 +0100

    Create ESP32_I2C_Slave.ino

---
+++ Projects/ESP32/ESP32_I2C_Slave.ino
@@ -0,0 +1,45 @@
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp32-i2c-master-slave-arduino/
+  ESP32 I2C Slave example: https://github.com/espressif/arduino-esp32/blob/master/libraries/Wire/examples/WireSlave/WireSlave.ino
+*********/
+
+#include "Wire.h"
+
+#define I2C_DEV_ADDR 0x55
+
+uint32_t i = 0;
+
+void onRequest() {
+  Wire.print(i++);
+  Wire.print(" Packets.");
+  Serial.println("onRequest");
+  Serial.println();
+}
+
+void onReceive(int len) {
+  Serial.printf("onReceive[%d]: ", len);
+  while (Wire.available()) {
+    Serial.write(Wire.read());
+  }
+  Serial.println();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Wire.onReceive(onReceive);
+  Wire.onRequest(onRequest);
+  Wire.begin((uint8_t)I2C_DEV_ADDR);
+
+#if CONFIG_IDF_TARGET_ESP32
+  char message[64];
+  snprintf(message, 64, "%lu Packets.", i++);
+  Wire.slaveWrite((uint8_t *)message, strlen(message));
+  Serial.print('Printing config %lu', i);
+#endif
+}
+
+void loop() {
+  
+}
