d65eb841deeda90d7edf3f56fb26633919337974
Rui Santos <randomnerdtutorials@gmail.com>
2024-07-17 16:38:00 +0100

    Update ESP32_Stepper_Motor_WS_Styled.ino

--- Projects/ESP32/ESP32_Stepper_Motor_WS_Styled/ESP32_Stepper_Motor_WS_Styled.ino
+++ Projects/ESP32/ESP32_Stepper_Motor_WS_Styled/ESP32_Stepper_Motor_WS_Styled.ino
@@ -75,7 +75,7 @@ void setup() {
   server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
     int params = request->params();
     for(int i=0;i<params;i++){
-      AsyncWebParameter* p = request->getParam(i);
+      const AsyncWebParameter* p = request->getParam(i);
       if(p->isPost()){
         // HTTP POST input1 value
         if (p->name() == PARAM_INPUT_1) {
