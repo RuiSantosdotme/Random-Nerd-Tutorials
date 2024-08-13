aa9ef6698a567277d571695b45fd6afd2f666d93
Rui Santos <randomnerdtutorials@gmail.com>
2024-08-13 17:12:23 +0100

    Create ESP32_NEO_6M_GPS_Basic_Serial.ino

---
+++ Projects/ESP32/ESP32_NEO_6M_GPS_Basic_Serial.ino
@@ -0,0 +1,34 @@
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete instructions at https://RandomNerdTutorials.com/esp32-neo-6m-gps-module-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+
+ // Define the RX and TX pins for Serial 2
+#define RXD2 16
+#define TXD2 17
+
+#define GPS_BAUD 9600
+
+// Create an instance of the HardwareSerial class for Serial 2
+HardwareSerial gpsSerial(2);
+
+void setup(){
+  // Serial Monitor
+  Serial.begin(115200);
+  
+  // Start Serial 2 with the defined RX and TX pins and a baud rate of 9600
+  gpsSerial.begin(GPS_BAUD, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("Serial 2 started at 9600 baud rate");
+}
+
+void loop(){
+  while (gpsSerial.available() > 0){
+    // get the byte data from the GPS
+    char gpsData = gpsSerial.read();
+    Serial.print(gpsData);
+  }
+  delay(1000);
+  Serial.println("-------------------------------");
+}
