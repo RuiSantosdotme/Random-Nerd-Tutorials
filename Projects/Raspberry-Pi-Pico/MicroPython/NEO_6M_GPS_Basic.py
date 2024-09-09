808fd1f84d444ef99b575b28fdb6eefca52f06f1
Rui Santos <randomnerdtutorials@gmail.com>
2024-09-09 21:51:26 +0100

    Create NEO_6M_GPS_Basic.py

---
+++ Projects/Raspberry-Pi-Pico/MicroPython/NEO_6M_GPS_Basic.py
@@ -0,0 +1,32 @@
+# Rui Santos & Sara Santos - Random Nerd Tutorials
+# Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-neo-6m-micropython/
+
+from machine import UART, Pin
+from time import sleep
+from micropyGPS import MicropyGPS
+
+# Instantiate the micropyGPS object
+my_gps = MicropyGPS()
+
+# Define the UART pins and create a UART object
+gps_serial = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+
+while True:
+    try:
+        while gps_serial.any():
+            data = gps_serial.read()
+            for byte in data:
+                stat = my_gps.update(chr(byte))
+                if stat is not None:
+                    # Print parsed GPS data
+                    print('UTC Timestamp:', my_gps.timestamp)
+                    print('Date:', my_gps.date_string('long'))
+                    print('Latitude:', my_gps.latitude_string())
+                    print('Longitude:', my_gps.longitude_string())
+                    print('Altitude:', my_gps.altitude)
+                    print('Satellites in use:', my_gps.satellites_in_use)
+                    print('Horizontal Dilution of Precision:', my_gps.hdop)
+                    print()
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
