e92df2fa7ef3a00e58f8aaf9713976501b28ef17
Rui Santos <randomnerdtutorials@gmail.com>
2024-09-26 15:17:37 +0100

    Create ESP32_Car_Robot_Web_Server.ino

---
+++ Projects/ESP32/ESP32_Car_Robot_Web_Server.ino
@@ -0,0 +1,188 @@
+/*  
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  https://RandomNerdTutorials.com/esp32-wi-fi-car-robot-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+#include <WiFi.h>
+#include <WebServer.h>
+
+// Replace with your network credentials
+const char* ssid     = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Create an instance of the WebServer on port 80
+WebServer server(80);
+
+// Motor 1
+int motor1Pin1 = 27; 
+int motor1Pin2 = 26; 
+int enable1Pin = 14;
+
+// Motor 2
+int motor2Pin1 = 33; 
+int motor2Pin2 = 25; 
+int enable2Pin = 32;
+
+// Setting PWM properties
+const int freq = 30000;
+const int resolution = 8;
+int dutyCycle = 0;
+
+String valueString = String(0);
+
+void handleRoot() {
+  const char html[] PROGMEM = R"rawliteral(
+  <!DOCTYPE HTML><html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:,">
+    <style>
+      html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center; }
+      .button { -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; background-color: #4CAF50; border: none; color: white; padding: 12px 28px; text-decoration: none; font-size: 26px; margin: 1px; cursor: pointer; }
+      .button2 {background-color: #555555;}
+    </style>
+    <script>
+      function moveForward() { fetch('/forward'); }
+      function moveLeft() { fetch('/left'); }
+      function stopRobot() { fetch('/stop'); }
+      function moveRight() { fetch('/right'); }
+      function moveReverse() { fetch('/reverse'); }
+
+      function updateMotorSpeed(pos) {
+        document.getElementById('motorSpeed').innerHTML = pos;
+        fetch(`/speed?value=${pos}`);
+      }
+    </script>
+  </head>
+  <body>
+    <h1>ESP32 Motor Control</h1>
+    <p><button class="button" onclick="moveForward()">FORWARD</button></p>
+    <div style="clear: both;">
+      <p>
+        <button class="button" onclick="moveLeft()">LEFT</button>
+        <button class="button button2" onclick="stopRobot()">STOP</button>
+        <button class="button" onclick="moveRight()">RIGHT</button>
+      </p>
+    </div>
+    <p><button class="button" onclick="moveReverse()">REVERSE</button></p>
+    <p>Motor Speed: <span id="motorSpeed">0</span></p>
+    <input type="range" min="0" max="100" step="25" id="motorSlider" oninput="updateMotorSpeed(this.value)" value="0"/>
+  </body>
+  </html>)rawliteral";
+  server.send(200, "text/html", html);
+}
+
+void handleForward() {
+  Serial.println("Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH); 
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
+  server.send(200);
+}
+
+void handleLeft() {
+  Serial.println("Left");
+  digitalWrite(motor1Pin1, LOW); 
+  digitalWrite(motor1Pin2, LOW); 
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
+  server.send(200);
+}
+
+void handleStop() {
+  Serial.println("Stop");
+  digitalWrite(motor1Pin1, LOW); 
+  digitalWrite(motor1Pin2, LOW); 
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);   
+  server.send(200);
+}
+
+void handleRight() {
+  Serial.println("Right");
+  digitalWrite(motor1Pin1, LOW); 
+  digitalWrite(motor1Pin2, HIGH); 
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);    
+  server.send(200);
+}
+
+void handleReverse() {
+  Serial.println("Reverse");
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW); 
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);          
+  server.send(200);
+}
+
+void handleSpeed() {
+  if (server.hasArg("value")) {
+    valueString = server.arg("value");
+    int value = valueString.toInt();
+    if (value == 0) {
+      ledcWrite(enable1Pin, 0);
+      ledcWrite(enable2Pin, 0);
+      digitalWrite(motor1Pin1, LOW); 
+      digitalWrite(motor1Pin2, LOW); 
+      digitalWrite(motor2Pin1, LOW);
+      digitalWrite(motor2Pin2, LOW);   
+    } else { 
+      dutyCycle = map(value, 25, 100, 200, 255);
+      ledcWrite(enable1Pin, dutyCycle);
+      ledcWrite(enable2Pin, dutyCycle);
+      Serial.println("Motor speed set to " + String(value));
+    }
+  }
+  server.send(200);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set the Motor pins as outputs
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
+
+  // Configure PWM Pins
+  ledcAttach(enable1Pin, freq, resolution);
+  ledcAttach(enable2Pin, freq, resolution);
+    
+  // Initialize PWM with 0 duty cycle
+  ledcWrite(enable1Pin, 0);
+  ledcWrite(enable2Pin, 0);
+  
+  // Connect to Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Define routes
+  server.on("/", handleRoot);
+  server.on("/forward", handleForward);
+  server.on("/left", handleLeft);
+  server.on("/stop", handleStop);
+  server.on("/right", handleRight);
+  server.on("/reverse", handleReverse);
+  server.on("/speed", handleSpeed);
+
+  // Start the server
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
