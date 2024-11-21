bc3353fbfe85e3751c464e78bdcb9b27a33b0b78
Rui Santos <randomnerdtutorials@gmail.com>
2024-11-21 13:38:30 +0000

    Create Pico_Asynchronous_Programming.py

---
+++ Projects/Raspberry-Pi-Pico/MicroPython/Pico_Asynchronous_Programming.py
@@ -0,0 +1,33 @@
+# Rui Santos & Sara Santos - Random Nerd Tutorials
+# Complete project details at https://RandomNerdTutorials.com/micropython-raspberry-pi-pico-asynchronous-programming/
+
+import asyncio
+from machine import Pin
+
+green_led_pin = 20
+green_led = Pin(green_led_pin, Pin.OUT)
+blue_led_pin = 19
+blue_led = Pin(blue_led_pin, Pin.OUT)
+
+# Define coroutine function
+async def blink_green_led():
+    while True:
+        green_led.toggle()
+        await asyncio.sleep(0.5) 
+
+# Define coroutine function
+async def blink_blue_led():
+    while True:
+        blue_led.toggle()
+        await asyncio.sleep(1)
+
+# Define the main function to run the event loop
+async def main():
+    # Create tasks for blinking two LEDs concurrently
+    asyncio.create_task(blink_green_led())
+    asyncio.create_task(blink_blue_led())
+
+# Create and run the event loop
+loop = asyncio.get_event_loop()  
+loop.create_task(main())  # Create a task to run the main function
+loop.run_forever()  # Run the event loop indefinitely
