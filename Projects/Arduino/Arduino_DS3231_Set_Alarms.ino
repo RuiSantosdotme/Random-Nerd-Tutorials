a77aae6701328512e698a85fe40a650b11b498fd
Rui Santos <randomnerdtutorials@gmail.com>
2025-03-28 10:19:52 +0000

    Update Arduino_DS3231_Set_Alarms.ino

--- Projects/Arduino/Arduino_DS3231_Set_Alarms.ino
+++ Projects/Arduino/Arduino_DS3231_Set_Alarms.ino
@@ -43,7 +43,7 @@ void printCurrentTime(){
 }
 
 void onAlarm() {
-  Serial.println("Alarm occured!");
+  Serial.println("Alarm occurred!");
   // toggle the current LED state
   int state = digitalRead(ledPin);
   digitalWrite(ledPin, !state);
