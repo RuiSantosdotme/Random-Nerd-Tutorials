15899fd96b2c3d9da9f63d04d945e5252de2cc61
Rui Santos <randomnerdtutorials@gmail.com>
2025-04-14 09:58:45 +0100

    Update ESP_Thermostat_Web_Server.ino

--- Projects/ESP/ESP_Thermostat_Web_Server.ino
+++ Projects/ESP/ESP_Thermostat_Web_Server.ino
@@ -1,12 +1,8 @@
 /*********
-  Rui Santos
+  Rui Santos & Sara Santos - Random Nerd Tutorials
   Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-thermostat-web-server/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 *********/
 
 #ifdef ESP32
@@ -108,7 +104,7 @@ void setup() {
   
   // Send web page to client
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
+    request->send(200, "text/html", index_html, processor);
   });
 
   // Receive an HTTP GET request at <ESP_IP>/get?threshold_input=<inputMessage>&enable_arm_input=<inputMessage2>
