263ef50781bd85b89e2e73d9b40e780fc354b503
Rui Santos <randomnerdtutorials@gmail.com>
2025-04-14 10:01:45 +0100

    Update ESP_Async_Web_Server_Display_Images_LittleFS.ino

--- Projects/ESP/ESP_Async_Web_Server_Display_Images_LittleFS.ino
+++ Projects/ESP/ESP_Async_Web_Server_Display_Images_LittleFS.ino
@@ -58,7 +58,7 @@ void setup(){
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
+    request->send(200, "text/html", index_html);
   });
   
   server.on("/sun", HTTP_GET, [](AsyncWebServerRequest *request){
