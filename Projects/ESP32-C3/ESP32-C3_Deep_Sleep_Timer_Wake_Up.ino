6568fd90a493ef093c75eaeda4668235e5057301
Rui Santos <randomnerdtutorials@gmail.com>
2025-07-28 11:23:38 +0100

    Update ESP32-C3_Deep_Sleep_Timer_Wake_Up.ino

--- Projects/ESP32-C3/ESP32-C3_Deep_Sleep_Timer_Wake_Up.ino
+++ Projects/ESP32-C3/ESP32-C3_Deep_Sleep_Timer_Wake_Up.ino
@@ -5,16 +5,16 @@
 
 #define uS_TO_S_FACTOR 1000000ULL   // Microseconds to seconds
 #define TIME_TO_SLEEP 60            // Deep sleep duration in seconds
-#define LED_PIN 4                   // LED connected to GPIO4
+#define LED_PIN 8                   // LED connected to GPIO 8
 
 void setup() {
   delay(100);
   pinMode(LED_PIN, OUTPUT);
 
-  // Blink LED
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
+  // Blink LED (on-board LED works with inverted logic)
   digitalWrite(LED_PIN, LOW);
+  delay(1000);
+  digitalWrite(LED_PIN, HIGH);
 
   // Set deep sleep timer
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
@@ -23,4 +23,6 @@ void setup() {
   esp_deep_sleep_start();
 }
 
-void loop() {}
+void loop() {
+  
+}
