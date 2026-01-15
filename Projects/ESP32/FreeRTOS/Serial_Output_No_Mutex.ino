fae82e41a62624b5b9ae80f6be7bcd97c84793e1
Rui Santos <randomnerdtutorials@gmail.com>
2026-01-15 14:53:28 +0000

    Create Serial_Output_No_Mutex.ino

---
+++ Projects/ESP32/FreeRTOS/Serial_Output_No_Mutex.ino
@@ -0,0 +1,53 @@
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp32-freertos-mutex-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+void Task1(void *parameter) {
+  for (;;) {
+    Serial.println("Task1: Logging from Task 1");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Serial.println("Task1: End of log from Task 1");
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+}
+
+void Task2(void *parameter) {
+  for (;;) {
+    Serial.println("Task2: Logging from Task 2");
+    vTaskDelay(800 / portTICK_PERIOD_MS);
+    Serial.println("Task2: End of log from Task 2");
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Starting FreeRTOS");
+
+  xTaskCreatePinnedToCore(
+    Task1,                  // Task function
+    "Task1",                // Task name
+    3000,                   // Stack size
+    NULL,                   // Task parameters
+    1,                      // Priority
+    NULL,                   // Task handle
+    1                       // Core ID
+  );
+
+  xTaskCreatePinnedToCore(
+    Task2,                  // Task function
+    "Task2",                // Task name
+    3000,                   // Stack size
+    NULL,                   // Task parameters
+    2,                      // Higher priority
+    NULL,                   // Task handle
+    1                       // Core ID
+  );
+}
+
+void loop() {
+
+}
